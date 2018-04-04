5bf11fa9dda04e9d74ad779ea8d3aeb4938ca78d
tomasen <tomasen@gmail.com>
2018-04-04 12:15:14 +0800

    add drag and drop handler

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -24,5 +24,32 @@ new Vue({
         this.$store.dispatch('togglePlayback');
       }
     });
+    window.addEventListener('drop', (e) => {
+      e.preventDefault();
+      const { files } = e.dataTransfer;
+      console.log(files);
+      // TODO: play it if it's video file
+      alert(`drag and drop are not yet supported.\nfile: ${files[0].path} ${files[0].name}`);
+      /*
+      for (const file in files) {
+        if (files.hasOwnProperty(file)) {
+          const filename = files[file].name
+          const fileExt = filename.substring(filename.lastIndexOf('.') + 1).toLowerCase()
+          if (Videos.allowedExtensions().indexOf(fileExt) !== -1) {
+            const video = {
+              id: videos.length + 1,
+              status: 'loading',
+              name: filename,
+              path: files[file].path,
+              size: files[file].size
+            }
+            videos.push(video)
+          }
+        }
+      } */
+    });
+    window.addEventListener('dragover', (e) => {
+      e.preventDefault();
+    });
   },
 }).$mount('#app');
