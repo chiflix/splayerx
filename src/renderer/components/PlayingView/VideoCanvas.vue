48d92580ee6cbb68ceca9c4228b1d05e45a574fb
tomasen <tomasen@gmail.com>
2018-04-06 03:27:44 +0800

    adding and using HTML Audio/Video DOM event and properties

--- src/renderer/components/PlayingView/VideoCanvas.vue
+++ src/renderer/components/PlayingView/VideoCanvas.vue
@@ -1,22 +1,34 @@
 <template>
   <video ref="videoCanvas"
-    preload="metadata" :src="src">
+    preload="metadata"
+    onplaying="onplaying"
+    onpause="onpause"
+    :src="src">
   </video>
 </template>;
 
 <script>
+// https://www.w3schools.com/tags/ref_av_dom.asp
+
 export default {
   data() {
     return {
     };
   },
   props: ['src'],
+  methods: {
+    onpause(e) {
+      console.log(e);
+    },
+    onplaying(e) {
+      console.log(e);
+    },
+  },
   created() {
     const self = this;
-    self.state = '';
     this.$bus.$on('toggle-playback', ($event) => {
       console.log('toggle-playback event has been triggered', $event);
-      if (self.state === 'pause') {
+      if (self.$refs.videoCanvas.paused) {
         self.$bus.$emit('play');
       } else {
         self.$bus.$emit('pause');
@@ -24,12 +36,10 @@ export default {
     });
     this.$bus.$on('play', ($event) => {
       console.log('play event has been triggered', $event);
-      self.state = 'play';
       self.$refs.videoCanvas.play();
     });
     this.$bus.$on('pause', ($event) => {
       console.log('pause event has been triggered', $event);
-      self.state = 'pause';
       self.$refs.videoCanvas.pause();
     });
   },
