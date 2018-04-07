0b04e7c066a75ab8de4dc69535981a96e5c4a83c
tomasen <tomasen@gmail.com>
2018-04-07 17:46:10 +0800

    move progressbar to components

--- src/renderer/components/PlayingView.vue
+++ src/renderer/components/PlayingView.vue
@@ -7,12 +7,7 @@
     <div class="video-controller" id="video-controller">
 			<div class="background"></div>
 			<div class="playstate" id="playstate"></div>
-			<div class="progress" id="progress">
-				<div class="progress--played" id="progress--played">
-					<div class="line" id="progress--line"></div>
-					<!-- <div class="light"></div> -->
-				</div>
-			</div>
+			<TimeProgressBar/>
       <TimeStatus/>
 			<div class="volume" id="volume">
 				<div class="volume--bar volume--mouseover">
@@ -35,12 +30,14 @@
 <script>
 import VideoCanvas from './PlayingView/VideoCanvas.vue';
 import TimeStatus from './PlayingView/TimeStatus.vue';
+import TimeProgressBar from './PlayingView/TimeProgressBar.vue';
 
 export default {
   name: 'playing-view',
   components: {
     VideoCanvas,
     TimeStatus,
+    TimeProgressBar,
   },
   methods: {
   },
@@ -92,35 +89,6 @@ export default {
     left: 0;
     width: 100%;
     height: 100%; }
-  .video-controller .progress {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 30px; }
-  .video-controller .progress--played {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 0; }
-    .video-controller .progress--played .line {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: 100%;
-      height: 2px;
-      background: #FFFFFF;
-      box-shadow: 0 0 20px 0 rgba(255, 255, 255, 0.5);
-      transition: height 200ms; }
-      .video-controller .progress--played .line.line--mouseover {
-        height: 10px; }
-    .video-controller .progress--played .light {
-      position: absolute;
-      bottom: 0;
-      width: 100%;
-      height: 300px;
-      opacity: 0.1;
-      background-image: linear-gradient(-180deg, rgba(255, 255, 255, 0) 76%, #FFFFFF 100%); }
 
   .video-controller .volume {
     position: absolute;
--- src/renderer/components/PlayingView/TimeProgressBar.vue
+++ src/renderer/components/PlayingView/TimeProgressBar.vue
@@ -1,10 +1,60 @@
 <template>
-  <div>
+  <div class="progress" id="progress">
+    <div class="progress--played" id="progress--played"
+    :style="{ width: progress +'%' }">
+      <div class="line" id="progress--line"></div>
+      <!-- <div class="light"></div> -->
+    </div>
   </div>
 </template>;
 
 <script>
+
+export default {
+  computed: {
+    progress() {
+      if (Number.isNaN(this.$store.state.PlaybackState.Duration)) {
+        return 0;
+      }
+      return (100 * this.$store.state.PlaybackState.CurrentTime)
+        / this.$store.state.PlaybackState.Duration;
+    },
+  },
+};
+
 </script>
 
-<style>
+<style lang="scss">
+
+.video-controller .progress {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 30px; }
+.video-controller .progress--played {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 0;
+}
+  .video-controller .progress--played .line {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 2px;
+    background: #FFFFFF;
+    box-shadow: 0 0 20px 0 rgba(255, 255, 255, 0.5);
+    transition: height 200ms; }
+    .video-controller .progress--played .line.line--mouseover {
+      height: 10px; }
+  .video-controller .progress--played .light {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 300px;
+    opacity: 0.1;
+    background-image: linear-gradient(-180deg, rgba(255, 255, 255, 0) 76%, #FFFFFF 100%); }
+
 </style>
