870108b5ea4a40215f2218c0660b3d57b642ffd9
tomasen <tomasen@gmail.com>
2018-04-07 18:36:20 +0800

    smooth progress bar transition

--- src/renderer/components/PlayingView/TimeProgressBar.vue
+++ src/renderer/components/PlayingView/TimeProgressBar.vue
@@ -31,30 +31,35 @@ export default {
   bottom: 0;
   left: 0;
   width: 100%;
-  height: 30px; }
+  height: 30px;
+}
 .video-controller .progress--played {
   position: absolute;
   bottom: 0;
   left: 0;
   width: 0;
+  transition: width 1.2s;
 }
-  .video-controller .progress--played .line {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 2px;
-    background: #FFFFFF;
-    box-shadow: 0 0 20px 0 rgba(255, 255, 255, 0.5);
-    transition: height 200ms; }
-    .video-controller .progress--played .line.line--mouseover {
-      height: 10px; }
-  .video-controller .progress--played .light {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 300px;
-    opacity: 0.1;
-    background-image: linear-gradient(-180deg, rgba(255, 255, 255, 0) 76%, #FFFFFF 100%); }
+.video-controller .progress--played .line {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background: #FFFFFF;
+  box-shadow: 0 0 20px 0 rgba(255, 255, 255, 0.5);
+  transition: height 200ms;
+}
+.video-controller .progress--played .line.line--mouseover {
+  height: 10px;
+}
+.video-controller .progress--played .light {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 300px;
+  opacity: 0.1;
+  background-image: linear-gradient(-180deg, rgba(255, 255, 255, 0) 76%, #FFFFFF 100%);
+}
 
 </style>
