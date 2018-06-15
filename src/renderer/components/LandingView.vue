6a1692a6d1b491a2df09d9f230df3d4285c73538
YuRiJin <875830716@qq.com>
2018-06-15 16:22:51 +0800

    接下Xia的设计继续做

--- src/renderer/components/LandingView.vue
+++ src/renderer/components/LandingView.vue
@@ -1,5 +1,10 @@
 <template>
 <div id="wrapper">
+ 
+  <img id='bg1' v-if='n==1' src="../assets/bg1.jpg">
+  <img id='bg2' v-if='n==2' src="../assets/bg2.jpg">
+  <img id='bg3' v-if='n==3' src="../assets/bg3.jpg">
+  <img id='bg4' v-if='n==4' src="../assets/bg4.jpg">
   <main>
     <div>
       <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
@@ -10,10 +15,13 @@
       <p> {{ version }} </p>
     </div>
 
-    <div class="controller">
-      <div class="playlist">
-        <a class="item" v-if="hasRecentPlaylist" href="#" @click="openFile(lastPlayedFile)">
-        </a>
+    <div class="controller"> 
+      <div class="playlist" >
+        <div class="recentplayed" v-if="hasRecentPlaylist">Recent played</div>
+        <a class="item" v-if="hasRecentPlaylist" href="#" @click="openFile(lastPlayedFile)"><img src="~@/assets/bg1.jpg" @mouseenter='fillImg(1)'></a>
+        <a class="item" v-if="hasRecentPlaylist" href="#" @click="openFile(lastPlayedFile)"><img src="~@/assets/bg2.jpg" @mouseenter='fillImg(2)'></a>
+        <a class="item" v-if="hasRecentPlaylist" href="#" @click="openFile(lastPlayedFile)"><img src="~@/assets/bg3.jpg" @mouseenter='fillImg(3)'></a>
+        <a class="item" v-if="hasRecentPlaylist" href="#" @click="openFile(lastPlayedFile)"><img src="~@/assets/bg4.jpg" @mouseenter='fillImg(4)'></a>
       </div>
       <button @click="open('./')">
         <img src="~@/assets/icon-open.svg" type="image/svg+xml">
@@ -31,6 +39,7 @@ export default {
       showingPopupDialog: false,
       lastPlayedFile: '',
       version: '',
+      n: '0',
     };
   },
   components: {
@@ -44,7 +53,6 @@ export default {
     const { app } = this.$electron.remote;
     this.version = app.getVersion();
     console.log(app.getVersion(), app.getName());
-
     this.$storage.get('recent-played', (err, data) => {
       if (err) {
         // TODO: proper error handle
@@ -91,6 +99,11 @@ export default {
         name: 'playing-view',
       });
     },
+    fillImg(arg) {
+      this.n = arg;
+      console.log('鼠标放入');
+      console.log(this.n);
+    },
   },
 };
 </script>
@@ -108,21 +121,34 @@ $themeColor-Light: white;
 body {
   color: $themeColor-Light;
 }
-
 #wrapper {
   background: radial-gradient( ellipse at top center,
   rgba(0, 0, 0, .9) 20%,
   rgba(44, 44, 44, .95) 80%);
   height: 100vh;
-  padding: 60px 80px;
+  padding: 0 80px;
   width: 100vw;
 }
-
+#wrapper #bg1, #wrapper #bg2, #wrapper #bg3, #wrapper #bg4 {
+  position: fixed;
+  top: 1px;
+  left: -1px;
+  z-index: 55;
+  // opacity:0.9;
+  width: 100%; 
+  height: 100%;  
+}
+// #wrapper .bgc{
+//   height: 100vh;
+//   width: 100vw;
+// }
 #logo {
+  position: relative;
   height: auto;
   margin-bottom: 20px;
   margin-top: 5vh;
   width: 20vw;
+  // z-index: 30;
 }
 
 main {
@@ -146,20 +172,62 @@ main>div {
 }
 
 .controller a {
+  // display: inline-block;
   color: #e4e4c4;
+  float: left;
+  width: 40vw;
+  height: 20vh;
+  margin: 40px 15px 40px 0px;
+}
+.controller a:first-child {
+  // display: inline-block;
+  margin-left: 180px;
+  // margin-left: -2000vh;
 }
 
+.controller a img{ 
+    width: 100%; 
+    height: 100%;  
+    max-width: 100%;  
+    max-height: 100%;
+    transition: all 0.15s;
+    // white-space: nowrap;
+    // display: flex;
+    // flex-wrap: nowrap;
+}
+.controller a img:hover{
+  transform: scale(1.4);
+  }
+
 .controller {
-  position: absolute;
-  right: 0;
-  bottom: 1em;
+  // position: absoulte;
+  // left: 0px;
+  // bottom: 1em;
+  margin-top: 30px;
+  margin-left: -45px;
   width: 100vw;
+  overflow: hidden;
+  // display: flex;
+  // flex-wrap: nowrap;
 }
 
 .controller .playlist {
+  position: relative;
   display: block;
-  margin-left: 1em;
+  // margin-left: 1em;
+  // margin-top: 30px;
+  // padding-left: 45px;
   float: left;
+  z-index: 100;
+}
+.controller .playlist .recentplayed{
+   position: absolute;
+  //  bottom: 3em;
+  //  left: 1em;
+  //  margin-left: -80vh;
+   color: white;
+   font-size: 2vw;
+   z-index: 100;
 }
 
 .controller .playlist .item {
@@ -182,6 +250,7 @@ main>div {
   outline: none;
   transition: all 0.15s ease;
   border: 0px;
+  z-index: 100;
 }
 
 .controller button.alt {
