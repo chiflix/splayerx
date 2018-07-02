f13b9c62de9e45d7145ba60b3f5f71c2241e7bba
Yvon Yan <yanyifeng@xindong.com>
2018-07-02 18:50:19 +0800

    [Feature|Add]: Add titlebar import statement

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app" class="application">
+    <titlebar></titlebar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import Titlebar from '@/components/Titlebar.vue';
   export default {
     name: 'splayer',
+    components: {
+      Titlebar,
+    },
   };
 </script>
 
