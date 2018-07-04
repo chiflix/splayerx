564df67870776ead8f6c8b42a5caeba4b163f6f6
hbin123 <hbin010@gmail.com>
2018-07-04 12:52:37 +0800

    update package.json

--- test/unit/specs/MediaQuickHash.spec.js
+++ test/unit/specs/MediaQuickHash.spec.js
@@ -1,13 +1,12 @@
 import helpers from '@/helpers';
 
-describe('MediaQuickHash test: ', function() {
-  it('should return correct hash value', function() {
+describe('MediaQuickHash test: ', function () {
+  it('should return correct hash value', function () {
     const expectedResult = '84f0e9e5e05f04b58f53e2617cc9c866-'
                         + 'f54d6eb31bef84839c3ce4fc2f57991c-'
                         + 'b1f0696aec64577228d93eabcc8eb69b-'
                         + 'f497c6684c4c6e50d0856b5328a4bedc';
-    const functionResult = helpers.methods.mediaQuickHash(
-      '../../../test/assets/mediaQuickHash_test.avi');
+    const functionResult = helpers.methods.mediaQuickHash('../../../test/assets/mediaQuickHash_test.avi');
     expect(functionResult).to.be.equal(expectedResult);
   });
 });
