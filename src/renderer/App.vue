eddd8e7c38c7a9271f0c05d6eae18b9c40ea70f4
Yvon Yan <yanyifeng@xindong.com>
2018-07-13 21:50:51 +0800

    [Optimize|Change]: Remove console.log and add eslint rule

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -10,22 +10,17 @@
     methods: {
       mainCommitProxy(commitType, commitPayload) {
         this.$store.commit(commitType, commitPayload);
-        console.log(`Main Commit ${commitType} with Payload ${commitPayload} success!`);
-        console.log(`Final state: ${this.$store.state.WindowState.windowSize}.`);
       },
       mainDispatchProxy(actionType, actionPayload) {
         this.$store.dispatch(actionType, actionPayload);
-        console.log(`Main Dispatch ${actionType} with Payload ${actionPayload} success!`);
-        console.log(`Final state: ${this.$store.state.WindowState.windowSize}.`);
       },
     },
     mounted() {
+      /* eslint-disable no-unused-vars */
       require('electron').ipcRenderer.on('mainCommit', (event, commitType, commitPayload) => {
-        console.log(event);
         this.mainCommitProxy(commitType, commitPayload);
       });
       require('electron').ipcRenderer.on('mainDispatch', (event, actionType, actionPayload) => {
-        console.log(event);
         this.mainDispatchProxy(actionType, actionPayload);
       });
     },
