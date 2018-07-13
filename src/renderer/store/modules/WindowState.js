4ff0550e7d62bc61ca0dbe4255d8b2dea80ceae3
Yvon Yan <yanyifeng@xindong.com>
2018-07-13 17:25:00 +0800

    [Optimize|Add]: Add WindowState to vuex

---
+++ src/renderer/store/modules/WindowState.js
@@ -0,0 +1,48 @@
+import { ipcRenderer } from 'electron';
+
+const state = {
+  windowSize: [0, 0],
+  windowPosition: [0, 0],
+};
+
+const getters = {
+  winWidth: state => state.windowSize[0],
+  winHeight: state => state.windowSize[1],
+  winSize: state => state.windowSize,
+  winPosX: state => state.windowPosition[0],
+  winPosY: state => state.windowPosition[1],
+  winPos: state => state.windowPosition,
+};
+
+const mutations = {
+  windowSize(state, payload) {
+    state.windowSize = payload;
+  },
+  windowPosition(state, payload) {
+    state.windowPosition = payload;
+  },
+};
+
+const actions = {
+  mainWindowSize(context, payload) {
+    context.commit('windowSize', payload);
+  },
+  rendererWindowSize(context, payload) {
+    ipcRenderer.send('windowSizeChange', payload);
+    context.commit('windowSize', payload);
+  },
+  mainWindowPosition(context, payload) {
+    context.commit('windowPosition', payload);
+  },
+  rendererWindowPosition(context, payload) {
+    ipcRenderer.send('windowPositionChange', payload);
+    context.commit('windowPosition', payload);
+  },
+};
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
+};
