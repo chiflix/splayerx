6ad6b576f94b1e5cbb9069db7aca75f67c912ef7
Qi Ning <qning1@sheffield.ac.uk>
2018-07-23 14:03:02 +0800

    unit test coverage - 19%

--- test/unit/specs/helpers_index.spec.js
+++ test/unit/specs/helpers_index.spec.js
@@ -2,16 +2,18 @@ import helpers from '@/helpers';
 
 describe('timecodeFromSeconds method works fine', () => {
   it('should display correct time codes', () => {
-
-    const expectArray = [ '00:00', '00:01', '00:10', '01:00', '01:01', '10:01',
-        '1:01:01', '11:11:11' ];
+    const expectArray = ['00:00', '00:01', '00:10', '01:00', '01:01', '10:01',
+      '1:01:01', '11:11:11'];
 
     const functionArray = [0, 1, 10, 60, 61, 601, 3661, 40271];
+    let i;
+    let expectedResult;
+    let functionResult;
 
-    for( var i=0;i<expectArray.length;i++ ){
-      var expectedResult = expectArray[i];
-      var functionResult = helpers.methods.timecodeFromSeconds(functionArray[i]);
+    for (i = 0; i < expectArray.length; i += 1) {
+      expectedResult = expectArray[i];
+      functionResult = helpers.methods.timecodeFromSeconds(functionArray[i]);
       expect(functionResult).to.be.equal(expectedResult);
-    };
+    }
   });
 });
