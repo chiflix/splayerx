bfefc2259b47d9f8d9e0f2f8cac7c72150e7887c
YuRiJin <875830716@qq.com>
2018-07-26 14:43:00 +0800

    删除注释

--- src/renderer/helpers/index.js
+++ src/renderer/helpers/index.js
@@ -91,52 +91,6 @@ export default {
         data = [newElement];
       }
       storage.setSync('recent-played', data);
-      // this.$storage.get('recent-played', (err, data) => {
-      //   console.log(data);
-      //   const newElement = {
-      //     path,
-      //     shortCut: '',
-      //     lastPlayedTime: 0,
-      //     duration: 0,
-      //   };
-      //   if (err) {
-      //     // TODO: proper error handle
-      //     this.$storage.set('recent-played', [newElement]);
-      //     console.log(err);
-      //   } else if (Array.isArray(data)) {
-      //     console.log('its an array!');
-      //     if (data.length < 4) {
-      //       if (this.$_indexOfExistedFileIn(data, path) === -1) {
-      //         data.unshift(newElement);
-      //       } else {
-      //         const item = data.splice(this.$_indexOfExistedFileIn(data, path), 1);
-      //         if (item[0].lastPlayedTime !== 0) {
-      //           this.$bus.$emit('seek', item[0].lastPlayedTime);
-      //         }
-      //         data.unshift(item[0]);
-      //       }
-      //       console.log('changed:');
-      //       console.log(data);
-      //       this.$storage.set('recent-played', data);
-      //     } else {
-      //       if (this.$_indexOfExistedFileIn(data, path) === -1) {
-      //         data.pop();
-      //         data.unshift(newElement);
-      //       } else {
-      //         const item = data.splice(this.$_indexOfExistedFileIn(data, path), 1);
-      //         if (item[0].lastPlayedTime !== 0) {
-      //           this.$bus.$emit('seek', item[0].lastPlayedTime);
-      //         }
-      //         data.unshift(item[0]);
-      //       }
-      //       console.log('changed:');
-      //       console.log(data);
-      //       this.$storage.set('recent-played', data);
-      //     }
-      //   } else {
-      //     this.$storage.set('recent-played', [newElement]);
-      //   }
-      // });
       this.$bus.$emit('new-file-open');
       this.$store.commit('SrcOfVideo', path);
       this.$router.push({
