1be39557eb78cbac4e1ca61bce69170dbab6cc60
Yvon Yan <yanyifeng@xindong.com>
2018-07-30 08:58:02 +0800

    [PV-D002|Add]: Add video player related constants

--- src/renderer/constants.js
+++ src/renderer/constants.js
@@ -15,3 +15,41 @@ export const PROGRESS_BAR_SLIDER_HIDE_HEIGHT = '4px';
 export const PROGRESS_BAR_HIDE_HEIGHT = '0px';
 
 export const FOOL_PROOFING_BAR_WIDTH = 20;
+
+export const DEFAULT_VIDEO_EVENTS = [
+  'abort',
+  'canplay',
+  'canplaythrough',
+  'durationchange',
+  'ended',
+  'loadeddata',
+  'loadedmetadata',
+  'loadstart',
+  'pause',
+  'play',
+  'playing',
+  'progress',
+  'ratechange',
+  'seeked',
+  'seeking',
+  'stalled',
+  'suspend',
+  'timeupdate',
+  'volumechange',
+  'waiting',
+];
+export const DEFAULT_VIDEO_OPTIONS = [
+  'autoplay',
+  'controls',
+  'crossOrigin',
+  'currentTime',
+  'defaultMuted',
+  'defaultPlaybackRate',
+  'loop',
+  'mediaGroup',
+  'muted',
+  'playbackRate',
+  'preload',
+  'src',
+  'volume',
+];
