f8d5fd11e9d7205d2e0180051865c223bb6349d5
Yvon Yan <yanyifeng@xindong.com>
2018-07-30 08:59:32 +0800

    [PV-D002|Add]: Add ThumbnailVideoPlayer

---
+++ src/renderer/components/PlayingView/ThumbnailVideoPlayer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="thumbnail-video-player">
+    <base-video-player
+      :src=src
+      :defaultEvents="['loadedmetadata', 'seeked']"
+      :customOptions="{ pauseOnStart: true }"
+      @loadedmetadata="onMetaLoaded"
+      @seeked="thumbnailGeneration"
+      ref="video" />
+  </div>
+</template>
+
+<script>
+import BaseVideoPlayer from './BaseVideoPlayer';
+export default {
+  name:'thumbnail-video-player',
+  components: {
+    BaseVideoPlayer,
+  },
+  props: {
+    src: String,
+  },
+  data() {
+    return {
+      videoElement: null,
+      count: 0,
+      generationInterval: 0,
+      generationFinished: false,
+    }
+  },
+  methods: {
+    onMetaLoaded() {
+      const videoInfo = {
+        duration: this.$refs.video.duration(),
+      }
+      this.$emit('video-info', videoInfo);
+    },
+    thumbnailGeneration() {
+
+    }
+  }
+}
+</script>
+
