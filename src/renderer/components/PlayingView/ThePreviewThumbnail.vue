f2936edec6d64f50a9152c69f7c7bed51e0e9f69
Yvon Yan <yanyifeng@xindong.com>
2018-08-07 19:07:04 +0800

    [PV-D002|Add]: Add ThePreviewThumbnail

---
+++ src/renderer/components/PlayingView/ThePreviewThumbnail.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="the-preview-thumbnail"
+    :style="{width: thumbnailWidth + 32 + 'px', left: positionOfThumbnail - 16 + 'px'}">
+    <thumbnail-video-player
+      :currentTime="currentTime"
+      :thumbnailWidth="thumbnailWidth"
+      :thumbnailHeight="thumbnailHeight"
+      :outerThumbnailInfo="outerThumbnailInfo"/>
+  </div>
+</template>
+
+<script>
+import ThumbnailVideoPlayer from './ThumbnailVideoPlayer';
+export default {
+  components: {
+    'thumbnail-video-player': ThumbnailVideoPlayer,
+  },
+  props: {
+    src: String,
+    currentTime: Number,
+    maxThumbnailWidth: Number,
+    videoRatio: Number,
+    thumbnailWidth: Number,
+    thumbnailHeight: Number,
+    positionOfThumbnail: Number,
+  },
+  data() {
+    return {
+      outerThumbnailInfo: {
+        videoSrc: this.src,
+        videoDuration: -1,
+        generationInterval: -1,
+        screenWidth: 1920,
+        maxThumbnailWidth: 240,
+        videoRatio: this.videoRatio,
+      },
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.the-preview-thumbnail {
+  position: absolute;
+  bottom: 20px;
+  -webkit-app-region: no-drag;
+}
+</style>
