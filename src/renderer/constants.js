ef812dd9135c4e5e533a18731add8207eddeb001
Yvon Yan <yanyifeng@xindong.com>
2018-08-13 20:21:16 +0800

    [PV-D002|Add]: Add info database constants

--- src/renderer/constants.js
+++ src/renderer/constants.js
@@ -55,3 +55,5 @@ export const DEFAULT_VIDEO_OPTIONS = [
 ];
 export const THUMBNAIL_DB_NAME = 'splayerx-preview-thumbnails';
 export const THUMBNAIL_DB_VERSION = 1;
+export const INFO_DATABASE_NAME = 'Info';
+export const INFO_DATABASE_VERSION = 1;
