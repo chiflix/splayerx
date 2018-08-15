c64a7cbd79d622e00102147a6589b0c7f38abe1e
huangbin <hbin010@gmail.com>
2018-08-15 21:38:10 +0800

    test(BaseSubtitle&mediaQuickHash): add unit test

--- test/unit/specs/BaseSubtitle.spec.js
+++ test/unit/specs/BaseSubtitle.spec.js
@@ -5,11 +5,12 @@ import { mount, createLocalVue } from '@vue/test-utils';
 import sinon from 'sinon';
 import fs from 'fs';
 import path from 'path';
+import srt2vtt from 'srt-to-vtt';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
-describe('BaseSubtitle.vue', () => {
+describe.only('BaseSubtitle.vue', () => {
   let store;
 
   beforeEach(() => {
@@ -26,9 +27,12 @@ describe('BaseSubtitle.vue', () => {
 
   it('should load correct data', () => {
     const wrapper = mount(BaseSubtitle, { store, localVue });
-    expect(wrapper.vm.startIndex).equal(0);
+    expect(wrapper.vm.textTrackID).equal(0);
     expect(wrapper.vm.firstSubIndex).equal(null);
-    expect(wrapper.vm.secondSubIndex).equal(null);
+    expect(wrapper.vm.Sagi).equal(null);
+    expect(wrapper.vm.mediaHash).equal('');
+    expect(wrapper.vm.firstActiveCues).deep.equal([]);
+    expect(wrapper.vm.secondActiveCues).deep.equal([]);
     expect(wrapper.vm.firstCueHTML).deep.equal([]);
     expect(wrapper.vm.secondCueHTML).deep.equal([]);
     expect(wrapper.vm.subNameArr).deep.equal([]);
@@ -41,24 +45,74 @@ describe('BaseSubtitle.vue', () => {
     expect(wrapper.vm.curStyle.background).equal('');
   });
 
-  it('concatStream error test', (done) => {
+  it('subtitleInitialize and load local subtitles', () => {
+  })
+
+  it('$_concatStream success test', (done) => {
     const wrapper = mount(BaseSubtitle, { store, localVue });
-    const callback = sinon.spy();
-    const subPath = '../../../test/assets/test3.srt';
-    const vttStream = fs.createReadStream(subPath);
-    wrapper.vm.concatStream(vttStream, callback);
-    done();
-    sinon.assert.calledOnce(callback);
+    const subPath = './test/assets/test3.srt';
+    const vttStream = fs.createReadStream(subPath).pipe(srt2vtt());
+    wrapper.vm.$_concatStream(vttStream, (err, res) => {
+      expect(err).equal(null);
+      done();
+    });
   });
 
-  it('$_subNameProcess test', () => {
+  it('$_concatStream error test', (done) => {
     const wrapper = mount(BaseSubtitle, { store, localVue });
-    const file = '../../../test/assets/test3.srt';
-    const result = wrapper.vm.$_subNameProcess(file);
+    const subPath = './test/assets/test3.srt';
+    const vttStream = fs.createReadStream(subPath).pipe(srt2vtt());
+    wrapper.vm.$nextTick(() => {
+      vttStream.emit('error', new Error('OOPS'));
+    });
+    wrapper.vm.$_concatStream(vttStream, (err, res) => {
+      expect(err).not.equal(null);
+      done();
+    });
+  });
+
+  it('$_timeProcess test', () => {
+    const wrapper = mount(BaseSubtitle, { store, localVue });
+    const res = wrapper.vm.$_timeProcess();
+    expect(res).equal(0);
+  });
+
+  it('$_subNameFromLocalProcess test', () => {
+    const wrapper = mount(BaseSubtitle, { store, localVue });
+    const file = './test/assets/test3.srt';
+    const result = wrapper.vm.$_subNameFromLocalProcess(file);
     const target = {
       title: path.parse(file).name,
       status: null,
+      textTrackID: 0,
+      origin: 'local',
     };
     expect(result).deep.equal(target);
   });
+
+  it('$_createSubtitleStream success', () => {
+    const wrapper = mount(BaseSubtitle, { store, localVue });
+    const subPath = './test/assets/test3.srt';
+    const spy = sinon.spy();
+    const concatStream = sinon.stub(wrapper.vm, '$_concatStream');
+    concatStream.yields();
+    wrapper.vm.$_createSubtitleStream(subPath, spy);
+    // done();
+    concatStream.restore();
+    sinon.assert.calledOnce(spy);
+  });
+
+  // it('addVttToVideoElement success', () => {
+  //   const wrapper = mount(BaseSubtitle, { store, localVue });
+  //   const files = ['./test/assets/test3.srt'];
+  //   const spy = sinon.spy();
+  //   const stub = sinon.stub(wrapper.vm, 'addVttToVideoElement');
+  //   // stub.yields();
+  //   // stub(files, spy);
+  //   // stub.restore();
+  //   // wrapper.vm.addVttToVideoElement(files, (err, res) => {
+  //   //   spy();
+  //   // });
+  //   // sinon.assert.calledOnce(spy);
+  // });
 });
