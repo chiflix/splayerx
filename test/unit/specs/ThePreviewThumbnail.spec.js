7f659126f82afa690b6a9c83f906f633e2b9db3b
Yvon Yan <yanyifeng@xindong.com>
2018-08-20 21:21:08 +0800

    test(the-preview-thumbnail): add currentTime switch test

--- test/unit/specs/ThePreviewThumbnail.spec.js
+++ test/unit/specs/ThePreviewThumbnail.spec.js
@@ -23,6 +23,10 @@ describe('Component - ThePreviewThumbnail', () => {
   beforeEach(() => {
     sandbox = sinon.createSandbox();
     wrapper = shallowMount(ThePreviewThumbnail, { propsData });
+    wrapper.vm.quickHash = '84f0e9e5e05f04b58f53e2617cc9c866-'
+                          + 'f54d6eb31bef84839c3ce4fc2f57991c-'
+                          + 'b1f0696aec64577228d93eabcc8eb69b-'
+                          + 'f497c6684c4c6e50d0856b5328a4bedc';
   });
   afterEach(() => {
     wrapper.destroy();
@@ -33,5 +37,8 @@ describe('Component - ThePreviewThumbnail', () => {
     wrapper.setProps({ src: 'http:///' });
 
     expect(wrapper.vm.mountVideo && wrapper.vm.mountImage).to.equal(false);
+  });
+  it('should switch currentTime update display and currentTimes', () => {
+    wrapper.setProps({ currentTime: 90 });
   });
 });
