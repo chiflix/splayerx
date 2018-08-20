f1f583f9b2c5ad2877f6af1092fd58b59ec430c4
Yvon Yan <yanyifeng@xindong.com>
2018-08-20 20:56:51 +0800

    test(the-preview-thumbnail): add src switching test

--- test/unit/specs/ThePreviewThumbnail.spec.js
+++ test/unit/specs/ThePreviewThumbnail.spec.js
@@ -11,4 +11,27 @@ describe('Component - ThePreviewThumbnail', () => {
 
     expect(wrapper.contains(ThePreviewThumbnail)).to.equal(true);
   });
+
+  const propsData = {
+    src: 'file:///',
+    currentTime: 0,
+    maxThumbnailWidth: 240,
+    videoRatio: 1.78,
+    thumbnailWidth: 135,
+    thumbnailHeight: 76,
+  };
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+    wrapper = shallowMount(ThePreviewThumbnail, { propsData });
+  });
+  afterEach(() => {
+    wrapper.destroy();
+    sandbox.restore();
+  });
+
+  it('should switch src reload ThumbnailDisplay and ThumbnailVideoPlayer', () => {
+    wrapper.setProps({ src: 'http:///' });
+
+    expect(wrapper.vm.mountVideo && wrapper.vm.mountImage).to.equal(false);
+  });
 });
