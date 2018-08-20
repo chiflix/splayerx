6a459ac6775d147da1068fc2245024e84a4c0ade
Yvon Yan <yanyifeng@xindong.com>
2018-08-20 16:37:24 +0800

    test(time-progress-bar): remove shallowMount

--- test/unit/specs/TimeProgressBar.spec.js
+++ test/unit/specs/TimeProgressBar.spec.js
@@ -2,7 +2,7 @@ import Vuex from 'vuex';
 import PlaybackState from '@/store/modules/PlaybackState';
 import WindowState from '@/store/modules/WindowState';
 import TimeProgressBar from '@/components/PlayingView/TimeProgressBar';
-import { mount, shallowMount, createLocalVue } from '@vue/test-utils';
+import { mount, createLocalVue } from '@vue/test-utils';
 import sinon from 'sinon';
 
 const localVue = createLocalVue();
