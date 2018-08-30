197a0fdbc13c25256ff2a910d21232995b098c29
Yvon Yan <yanyifeng@xindong.com>
2018-08-30 16:29:56 +0800

    refactor(playing-view): move video-controller logic to another component

--- src/renderer/components/PlayingView.vue
+++ src/renderer/components/PlayingView.vue
@@ -1,155 +1,21 @@
 <template>
-  <div
-    :data-component-name="$options.name"
-    class="player"
-    :style="{ cursor: cursorStyle }">
-    <VideoCanvas :src="uri" ref="VideoCanvasRef"/>
-    <div class="masking"
-      v-show="showMask"></div>
-    <the-video-controller />
-    <!-- <div class="video-controller" id="video-controller"
-      @mousedown.self="resetDraggingState"
-      @mousedown.right.stop="handleRightClick"
-      @mousedown.left.stop.prevent="handleLeftClick"
-      @mouseup.left.prevent.self="handleMouseUp"
-      @mousewheel="wheelVolumeControll"
-      @mouseleave="mouseleaveHandler"
-      @mousemove.self="handleMouseMove"
-      @mouseenter="mouseEnter">
-      <PlayButton/>
-      <titlebar currentView="Playingview"></titlebar>
-      <TimeProgressBar :src="uri" />
-      <TheTimeCodes/>
-      <div class="control-buttons">
-        <VolumeControl class="button volume" />
-        <SubtitleControl class="button subtitle" />
-        <AdvanceControl class="button advance"/>
-      </div>
-    </div> -->
+  <div :data-component-name="$options.name" class="player" @mousewheel="wheelVolumeControll">
+    <the-video-canvas :src="uri" ref="VideoCanvasRef"/>
+    <the-video-controller/>
   </div>
 </template>
 
 <script>
-import TheVideoController from './PlayingView/TheVideoController';
-import Titlebar from './Titlebar.vue';
 import VideoCanvas from './PlayingView/VideoCanvas.vue';
-import TheTimeCodes from './PlayingView/TheTimeCodes.vue';
-import TimeProgressBar from './PlayingView/TimeProgressBar.vue';
-import VolumeControl from './PlayingView/VolumeControl.vue';
-import AdvanceControl from './PlayingView/AdvanceControl.vue';
-import SubtitleControl from './PlayingView/SubtitleControl.vue';
-import PlayButton from './PlayingView/PlayButton.vue';
-import UnfousedHelper from './PlayingView/helpers/macUnfocusHelper.js';
+import TheVideoController from './PlayingView/TheVideoController';
 
 export default {
   name: 'playing-view',
   components: {
     'the-video-controller': TheVideoController,
-    VideoCanvas,
-    TheTimeCodes,
-    TimeProgressBar,
-    VolumeControl,
-    AdvanceControl,
-    SubtitleControl,
-    Titlebar,
-    PlayButton,
-  },
-  data() {
-    return {
-      leave: false,
-      isDragging: false,
-      showMask: false,
-      cursorShow: true,
-      popupShow: false,
-      mouseDown: false,
-      timeoutIdOfAllWidgetsDisappearDelay: 0,
-      // the following 3 properties are used for checking if an event is a click or an dblclick
-      // during 200miliseconds, if a second click is detected, will toggle "FullScreen"
-      delay: 200, // changable and should be discussed.
-      clicks: 0,
-      timer: null,
-      mainWindow: null,
-      unfocusedHelper: null,
-      dragRadiusSquare: 25,
-      dragTime: 200,
-      mouseDownTime: null,
-      mouseDownCursorPosition: null,
-      subtitleMenuAppear: false,
-    };
-  },
-  created() {
-    this.mainWindow = this.$electron.remote.getCurrentWindow();
+    'the-video-canvas': VideoCanvas,
   },
   methods: {
-    toggleFullScreenState() {
-      const currentWindow = this.$electron.remote.getCurrentWindow();
-      if (currentWindow.isFullScreen()) {
-        currentWindow.setFullScreen(false);
-        this.$bus.$emit('reset-windowsize');
-      } else {
-        currentWindow.setAspectRatio(0);
-        currentWindow.setFullScreen(true);
-      }
-    },
-    wakeUpAllWidgets() {
-      if (!this.leave) {
-        this.showMask = true;
-        this.isDragging = true;
-        this.cursorShow = true;
-        this.$bus.$emit('volumecontroller-appear');
-        this.$bus.$emit('progressbar-appear');
-        this.$bus.$emit('timecode-appear');
-        this.$bus.$emit('sub-ctrl-appear');
-        this.$bus.$emit('titlebar-appear');
-        this.$bus.$emit('advance-appear');
-        if (this.timeoutIdOfAllWidgetsDisappearDelay !== 0) {
-          clearTimeout(this.timeoutIdOfAllWidgetsDisappearDelay);
-          this.timeoutIdOfAllWidgetsDisappearDelay
-              = setTimeout(this.hideAllWidgets, 3000);
-        } else {
-          this.timeoutIdOfAllWidgetsDisappearDelay
-              = setTimeout(this.hideAllWidgets, 3000);
-        }
-      }
-      this.leave = false;
-    },
-    mouseEnter() {
-      console.log(document.hidden);
-      if (this.unfocusedHelper.needHandle()) {
-        this.wakeUpAllWidgets();
-      }
-    },
-    mouseleaveHandler() {
-      if (!this.unfocusedHelper.needHandle()) return;
-      this.leave = true;
-      if (this.timeoutIdOfAllWidgetsDisappearDelay !== 0) {
-        clearTimeout(this.timeoutIdOfAllWidgetsDisappearDelay);
-        this.timeoutIdOfAllWidgetsDisappearDelay
-            = setTimeout(this.hideAllWidgets, 1500);
-      } else {
-        this.timeoutIdOfAllWidgetsDisappearDelay
-            = setTimeout(this.hideAllWidgets, 1500);
-      }
-    },
-    hideAllWidgets() {
-      this.showMask = false;
-      this.$bus.$emit('volumecontroller-hide');
-      this.$bus.$emit('progressbar-hide');
-      this.$bus.$emit('timecode-hide');
-      if (process.platform !== 'win32') {
-        this.$bus.$emit('titlebar-hide');
-      }
-      this.$bus.$emit('sub-ctrl-hide');
-      this.$bus.$emit('titlebar-hide');
-      this.$bus.$emit('advance-hide');
-      this.cursorShow = false;
-    },
-    resetDraggingState() {
-      this.isDragging = false;
-    },
-    togglePlayback() {
-      this.$bus.$emit('toggle-playback');
-    },
     wheelVolumeControll(e) {
       this.$bus.$emit('volumecontroller-appear-delay');
       this.$bus.$emit('volumeslider-appear');
@@ -173,87 +39,14 @@ export default {
         }
       }
     },
-    handleRightClick() {
-      if (process.platform !== 'darwin') {
-        const menu = this.$electron.remote.Menu.getApplicationMenu();
-        menu.popup(this.$electron.remote.getCurrentWindow());
-        this.popupShow = true;
-      }
-    },
-    handleLeftClick() {
-      this.mouseDownCursorPosition = this.$electron.screen.getCursorScreenPoint();
-      this.mouseDownTime = new Date();
-      const menu = this.$electron.remote.Menu.getApplicationMenu();
-      if (this.popupShow === true) {
-        menu.closePopup();
-        this.popupShow = false;
-      }
-      // Handle dragging-related variables
-      this.mouseDown = true;
-      return false;
-    },
-    handleMouseMove() {
-      if (!this.unfocusedHelper.needHandle()) return;
-      this.wakeUpAllWidgets();
-    },
-    handleMouseUp() {
-      if (this.subtitleMenuAppear) {
-        this.subtitleMenuAppear = false;
-        this.$bus.$emit('subtitle-menu-off');
-      } else {
-        this.mouseDown = false;
-        this.clicks += 1; // one click(mouseUp) triggered, clicks + 1
-        if (this.clicks === 1) { // if one click has been detected - clicks === 1
-          const self = this; // define a constant "self" for the following scope to use
-          if (this.isValidClick()) {
-            this.timer = setTimeout(() => { // define timer as setTimeOut function
-              self.togglePlayback(); // which is togglePlayback
-              self.clicks = 0; // reset the "clicks" to zero for next event
-            }, this.delay);
-          } else {
-            self.clicks = 0;
-          }
-        } else { // else, if a second click has been detected - clicks === 2
-          clearTimeout(this.timer); // cancel the time out
-          this.toggleFullScreenState();
-          this.clicks = 0;// reset the "clicks" to zero
-        }
-      }
-    },
-    isValidClick() { // this check will be at on mouse up
-      const cp = this.$electron.screen.getCursorScreenPoint();
-      if (new Date() - this.mouseDownTime > this.dragTime) {
-        return false;
-      }
-      const radiusSquare = ((cp.x - this.mouseDownCursorPosition.x) ** 2) +
-          ((cp.y - this.mouseDownCursorPosition.y) ** 2);
-      if (radiusSquare - this.dragRadiusSquare > 0) {
-        return false;
-      }
-      return true;
-    },
   },
   mounted() {
-    this.unfocusedHelper = new (UnfousedHelper())(this.mainWindow, this);
     this.$bus.$emit('play');
     this.$electron.remote.getCurrentWindow().setResizable(true);
-    this.$bus.$on('clear-all-widget-disappear-delay', () => {
-      clearTimeout(this.timeoutIdOfAllWidgetsDisappearDelay);
-    });
-    this.$bus.$on('hide-all-widgets', this.hideAllWidgets);
-    this.$bus.$on('subtitle-menu-toggled-on', () => {
-      this.subtitleMenuAppear = true;
-    });
-    this.$bus.$on('subtitle-menu-toggled-off', () => {
-      this.subtitleMenuAppear = false;
-    });
   },
   computed: {
     uri() {
       return this.$store.state.PlaybackState.SrcOfVideo;
-    },
-    cursorStyle() {
-      return this.cursorShow ? 'default' : 'none';
     },
   },
 };
@@ -264,97 +57,5 @@ export default {
   width: 100%;
   height: 100%;
   background-color: black;
-}
-.masking {
-  position: absolute;
-  left: 0;
-  bottom: 0;
-  width: 100%;
-  height: 50%;
-  opacity: 0.3;
-  background-image: linear-gradient(
-    -180deg,
-    rgba(0, 0, 0, 0) 0%,
-    rgba(0, 0, 0, 0.19) 62%,
-    rgba(0, 0, 0, 0.29) 100%
-  );
-}
-/*
- * Controller
- * 视频控制组件
- */
-.video-controller {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: 4px;
-  opacity: 1;
-  transition: opacity 400ms;
-}
-.control-buttons {
-  display: flex;
-  justify-content: space-between;
-  position: fixed;
-  .button {
-    -webkit-app-region: no-drag;
-    cursor: pointer;
-    position: relative;
-  }
-  .subtitle {
-    order: 1;
-  }
-  .volume {
-    order: 2;
-  }
-  .advance {
-    order: 3;
-  }
-  img {
-    width: 100%;
-    height: 100%;
-  }
-}
-@media screen and (max-width: 512px) {
-  .control-buttons {
-    display: none;
-  }
-}
-@media screen and (min-width: 513px) and (max-width: 854px) {
-  .control-buttons {
-    width: 119px;
-    height: 18px;
-    right: 27px;
-    bottom: 20px;
-    .button {
-      width: 23px;
-      height: 18px;
-    }
-  }
-}
-@media screen and (min-width: 855px) and (max-width: 1920px) {
-  .control-buttons {
-    width: 159px;
-    height: 24px;
-    right: 32px;
-    bottom: 24px;
-    .button {
-      width: 30.67px;
-      height: 24px;
-    }
-  }
-}
-@media screen and (min-width: 1921px) {
-  .control-buttons {
-    width: 238px;
-    height: 36px;
-    right: 48px;
-    bottom: 35px;
-    .button {
-      width: 46px;
-      height: 36px;
-    }
-  }
 }
 </style>
