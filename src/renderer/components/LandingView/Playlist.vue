faebf70422663313f0863720cb9f3507716dc095
Yvon Yan <yanyifeng@xindong.com>
2018-09-27 19:47:50 +0800

    refactor(playlist): display small image in Playlist

--- src/renderer/components/LandingView/Playlist.vue
+++ src/renderer/components/LandingView/Playlist.vue
@@ -27,7 +27,7 @@
         :class="showShadow ? 'shadow' : '' "
         :style="{
           transition: tranFlag ? 'width 150ms ease-out, height 150ms ease-out, border 150ms ease-out' : '',
-          backgroundImage: itemShortcut(item.shortCut),
+          backgroundImage: itemShortcut(item.smallShortCut),
           width: item.chosen ? `${itemWidth * 9 / 7}px` : `${itemWidth}px`,
           height: item.chosen ? `${itemHeight * 9 / 7}px` : `${itemHeight}px`,
         }"
@@ -128,7 +128,6 @@ export default {
       this.$bus.$emit('move', this.move);
     };
   },
-
   watch: {
     windowWidth(val) {
       this.itemWidth = (this.changeSize * val) / 100;
