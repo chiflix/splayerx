a81f6d85e5594d5ca1c4ee9c9f5209eb76f09a28
Yvon Yan <yanyifeng@xindong.com>
2018-10-10 15:19:50 +0800

    refactor(vuex/types): add types for actions and mutations

---
+++ src/renderer/store/mutation-types.js
@@ -0,0 +1,9 @@
+export const Video = {
+  VOLUME_UPDATE: 'VOLUME_UPDATE',
+  TOGGLE_MUTE: 'TOGGLE_MUTE',
+  RATE_UPDATE: 'RATE_UPDATE',
+};
+
+export const Subtitle = {
+  ADD_SUBTITLE: 'ADD_SUBTITLE',
+};
