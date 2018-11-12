adf866d79ec86a2cd59aca41e7009eeee7411e38
Sin <dev@zouxin.net>
2018-11-12 22:19:47 +0800

    fix(windows): fix window size changes while dragging if dpr>1

--- src/renderer/helpers/drag.js
+++ src/renderer/helpers/drag.js
@@ -1,4 +1,4 @@
-import { ipcRenderer } from 'electron'; //eslint-disable-line
+import { ipcRenderer, remote } from 'electron'; //eslint-disable-line
 
 let mouseConstructor = null;
 
@@ -30,11 +30,15 @@ export default function drag(element) {
   if (!mouseConstructor) return () => {};
   const mouse = mouseConstructor();
   let offset = null;
+  let windowSize = null;
   const onmousedown = (e) => {
     if (parentsHasClass(e.target, 'no-drag')) {
       offset = null;
     } else {
       offset = [e.clientX, e.clientY];
+    }
+    if (getRatio() !== 1) {
+      windowSize = remote.getCurrentWindow().getSize();
     }
   };
 
@@ -45,10 +49,14 @@ export default function drag(element) {
     x = Math.round((x / getRatio()) - offset[0]);
     y = Math.round((y / getRatio()) - offset[1]);
     ipcRenderer.send('callCurrentWindowMethod', 'setPosition', [x, y]);
+    if (windowSize) {
+      ipcRenderer.send('callCurrentWindowMethod', 'setSize', windowSize);
+    }
   });
 
   mouse.on('left-up', () => {
     offset = null;
+    windowSize = null;
   });
 
   return () => {
