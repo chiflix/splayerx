99a322fc1f829c407e5099cee14d555018e8ad8b
YuRiJin <875830716@qq.com>
2018-11-26 22:26:35 +0800

    fix(recentplaylist): fix error log

--- src/renderer/components/PlayingView/RecentPlaylist.vue
+++ src/renderer/components/PlayingView/RecentPlaylist.vue
@@ -164,7 +164,7 @@ export default {
     },
     mousemove(val) {
       const distance = this.winWidth > 1355 ? 20 : 10;
-      if (!this.canHoverItem) {
+      if (!this.canHoverItem && this.displayState) {
         if (Math.abs(this.mousePosition[0] - val.position[0]) > distance ||
         Math.abs(this.mousePosition[1] - val.position[1]) > distance) {
           this.canHoverItem = true;
