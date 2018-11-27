ceb04e2eadd77bd79b5119e3e66b5937ff1f87c5
YuRiJin <875830716@qq.com>
2018-11-27 21:01:50 +0800

    feat(notificationbubble): provide an event for calling privacy confirm

--- src/renderer/components/NotificationBubble.vue
+++ src/renderer/components/NotificationBubble.vue
@@ -70,6 +70,9 @@ export default {
         this.showPrivacyBubble = false;
       }
     });
+    this.$bus.$on('privacy-confirm', () => {
+      this.showPrivacyBubble = true;
+    });
   },
   methods: {
     closePrivacyBubble() {
