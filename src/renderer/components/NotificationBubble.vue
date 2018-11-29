86401a9b44f6af691e556cb28212071b4995d896
YuRiJin <875830716@qq.com>
2018-11-29 18:55:43 +0800

    refator(errorbubble): add basic layer on loading bubble

--- src/renderer/components/NotificationBubble.vue
+++ src/renderer/components/NotificationBubble.vue
@@ -15,15 +15,14 @@
       <div v-for="m in messages" :key="m.id"
         class="messageContainer"
         :id="'item' + m.id">
-        <div class="black-gradient">
-          <div
-          :class="m.type === 'error' ? 'errorContainer' : 'loadingContainer'">
-            <div class="bubbleContent">
-              <div class="title" v-if="m.type === 'error'">{{ m.title }}</div>
-              <div class="content">{{ m.content }}</div>
-            </div>
-            <Icon v-if="m.type === 'error'" type="close" class="bubbleClose" @click.native.left="closeMessage(m.id)"></Icon>
+        <div :class="m.type === 'error' ? 'black-gradient-error' : 'black-gradient-loading'"/>
+        <div
+        :class="m.type === 'error' ? 'errorContainer' : 'loadingContainer'">
+          <div class="bubbleContent">
+            <div class="title" v-if="m.type === 'error'">{{ m.title }}</div>
+            <div class="content">{{ m.content }}</div>
           </div>
+          <Icon v-if="m.type === 'error'" type="close" class="bubbleClose" @click.native.left="closeMessage(m.id)"></Icon>
         </div>
       </div>
     </transition-group>
@@ -292,24 +291,76 @@ export default {
   transition: 400ms cubic-bezier(0.17, 0.67, 0.17, 0.98);
   transition-property: opacity, transform;
 }
-.black-gradient {
+.black-gradient-error {
+  position: absolute;
   background-color: rgba(0,0,0,0.20);
   backdrop-filter: blur(9.6px);
   box-shadow: 0 0 2px 0 rgba(0,0,0,0.30);
-  @media screen and (max-width: 512px) {
-    display: none;
+  @media screen and (min-width: 320px) and (max-width: 512px) {
+    width: 216px;
+    height: 47px;
+    margin-bottom: 8px;
+    border-radius: 6px;
+    clip-path: inset(0 round 6px);
   }
   @media screen and (min-width: 513px) and (max-width: 854px) {
+    width: 240px;
+    height: 52px;
+    margin-bottom: 12px;
     border-radius: 7px;
-    clip-path: inset(0px round 7px);
+    clip-path: inset(0 round 7px);
   }
   @media screen and (min-width: 855px) and (max-width: 1920px) {
-    border-radius: 8.4px;
-    clip-path: inset(0px round 8.4px);
+    width: 288px;
+    height: 62px;
+    margin-bottom: 15px;
+    border-radius: 8px;
+    clip-path: inset(0 round 8px);
   }
   @media screen and (min-width: 1921px) {
-    border-radius: 11.76px;
-    clip-path: inset(0px round 11.76px);
+    width: 403px;
+    height: 87px;
+    margin-bottom: 18px;
+    border-radius: 11px;
+    clip-path: inset(0 round 11px);
+  }
+}
+.black-gradient-loading {
+  position: absolute;
+  background-color: rgba(0,0,0,0.20);
+  backdrop-filter: blur(9.6px);
+  box-shadow: 0 0 2px 0 rgba(0,0,0,0.30);
+  @media screen and (min-width: 320px) and (max-width: 512px) {
+    width: 136px;
+    height: 32px;
+    margin-left: 80px;
+    margin-bottom: 8px;
+    border-radius: 6px;
+    clip-path: inset(0 round 6px);
+  }
+  @media screen and (min-width: 513px) and (max-width: 854px) {
+    width: 148px;
+    height: 36px;
+    margin-left: 92px;
+    margin-bottom: 12px;
+    border-radius: 7px;
+    clip-path: inset(0 round 7px);
+  }
+  @media screen and (min-width: 855px) and (max-width: 1920px) {
+    width: 182px;
+    height: 43px;
+    margin-left: 106px;
+    margin-bottom: 15px;
+    border-radius: 8px;
+    clip-path: inset(0 round 8px);
+  }
+  @media screen and (min-width: 1921px) {
+    width: 256px;
+    height: 60px;
+    margin-left: 147px;
+    margin-bottom: 18px;
+    border-radius: 11px;
+    clip-path: inset(0 round 11px);
   }
 }
 
