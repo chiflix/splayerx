400172437f2af7a09f1f6483fac8587305ab531e
Tan Yang <1029862865@qq.com>
2018-11-30 18:40:52 +0800

    test(test): delete error test

--- test/unit/specs/SubtitleControl.spec.js
+++ test/unit/specs/SubtitleControl.spec.js
@@ -3,7 +3,6 @@ import Video from '@/store/modules/Video';
 import Subtitle from '@/store/modules/Subtitle';
 import SubtitleControl from '@/components/PlayingView/SubtitleControl';
 import { shallowMount, createLocalVue } from '@vue/test-utils';
-import sinon from 'sinon';
 
 const localVue = createLocalVue();
 localVue.use(Vuex);
@@ -28,31 +27,6 @@ describe('SubtitleControl.vue', () => {
     });
   });
 
-  it('toggleItemClick method works fine', () => {
-    const wrapper = shallowMount(SubtitleControl, {
-      store,
-      localVue,
-      attachToDocument: true,
-    });
-    const spy = sinon.spy(wrapper.vm.$bus, '$emit');
-    wrapper.vm.toggleItemClick(1);
-    expect(wrapper.vm.currentSubIden).equal(1);
-    expect(spy.calledOnce).equal(true);
-    expect(spy.firstCall.args[0]).equal('sub-first-change');
-    expect(spy.firstCall.args[1]).equal(1);
-    spy.restore();
-  });
-
-  it('toggleSubtitleOff method works fine', () => {
-    const wrapper = shallowMount(SubtitleControl, { store, localVue });
-    const spy = sinon.spy(wrapper.vm.$bus, '$emit');
-    wrapper.vm.toggleSubtitleOff();
-    expect(wrapper.vm.currentSubIden).equal(-1);
-    expect(spy.calledOnce).equal(true);
-    expect(spy.firstCall.args.length).equal(1);
-    expect(spy.firstCall.args[0]).equal('first-subtitle-off');
-    spy.restore();
-  });
 
   it('itemHasBeenChosen method works fine', () => {
     const wrapper = shallowMount(SubtitleControl, { store, localVue });
@@ -73,32 +47,11 @@ describe('SubtitleControl.vue', () => {
     expect(wrapper.vm.itemTitleHasChineseChar('旰')).equal(true);
   });
 
-  it('toggleLoadServerSubtitles method work fine', () => {
-    const wrapper = shallowMount(SubtitleControl, { store, localVue });
-    const spy = sinon.spy(wrapper.vm.$bus, '$emit');
-    wrapper.vm.toggleLoadServerSubtitles();
-    expect(spy.calledOnce).equal(true);
-    expect(spy.args[0][0]).equal('load-server-transcripts');
-    spy.restore();
-  });
 
   it('toggleOpenFileDialog method work fine if dialog is showing', () => {
     const wrapper = shallowMount(SubtitleControl, { store, localVue });
     wrapper.setData({ showingPopupDialog: true });
     wrapper.vm.toggleOpenFileDialog();
     expect(wrapper.vm.showingPopupDialog).equal(true);
-  });
-
-  it('added-local-subtitles event listener work fine', () => {
-    const wrapper = shallowMount(SubtitleControl, { store, localVue });
-    wrapper.setData = {
-      foundSubtitles: false,
-      currentSubIden: -1,
-    };
-    wrapper.vm.$bus.$emit('added-local-subtitles', 9);
-    const stub = sinon.stub(wrapper.vm.$bus, '$on');
-    stub('added-local-subtitles');
-    expect(wrapper.vm.foundSubtitles).equal(true);
-    expect(wrapper.vm.currentSubIden).equal(9);
   });
 });
