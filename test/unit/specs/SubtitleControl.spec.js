bea626e060436d9eeb3dbf95b6696284a585af08
Tan Yang <1029862865@qq.com>
2018-12-04 14:42:02 +0800

    test(subtitlecontrol): fix unit test

--- test/unit/specs/SubtitleControl.spec.js
+++ test/unit/specs/SubtitleControl.spec.js
@@ -1,3 +1,8 @@
-describe('SubtitleControl.vue', () => {
+import Vuex from 'vuex';
+import { createLocalVue } from '@vue/test-utils';
 
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('SubtitleControl.vue', () => {
 });
