e1eaeccd97cfefd76f8daf97808a8abd6aca2d7c
Tan Yang <1029862865@qq.com>
2018-12-04 14:31:55 +0800

    test(subtitlecontrol): delete useless test

--- src/renderer/components/PlayingView/SubtitleControl.vue
+++ src/renderer/components/PlayingView/SubtitleControl.vue
@@ -334,43 +334,6 @@ export default {
       this.currentSubIden = -1;
       this.offCurrentSubtitle();
     },
-    toggleLoadServerSubtitles() {
-      this.$bus.$emit('load-server-transcripts');
-    },
-    toggleOpenFileDialog() {
-      if (this.showingPopupDialog) {
-        return;
-      }
-      const self = this;
-      const { remote } = this.$electron;
-      const { dialog } = remote;
-      const browserWindow = remote.BrowserWindow;
-      const focusWindow = browserWindow.getFocusedWindow();
-      const VALID_EXTENSION = ['ass', 'srt', 'vtt'];
-
-      self.showingPopupDialog = true;
-      dialog.showOpenDialog(focusWindow, {
-        title: 'Open Dialog',
-        defaultPath: './',
-        filters: [{
-          name: 'Subtitle Files',
-          extensions: VALID_EXTENSION,
-        }],
-        properties: ['openFile'],
-      }, (item) => {
-        self.showingPopupDialog = false;
-        if (item) {
-          self.$bus.$emit('add-subtitle', item);
-        }
-      });
-    },
-    itemHasBeenChosen(index = 0) {
-      return index === this.currentSubIden;
-    },
-    itemTitleHasChineseChar(str) {
-      const REGEX_CHINESE = /[\u4e00-\u9fff]|[\u3400-\u4dbf]|[\u{20000}-\u{2a6df}]|[\u{2a700}-\u{2b73f}]|[\u{2b740}-\u{2b81f}]|[\u{2b820}-\u{2ceaf}]|[\uf900-\ufaff]|[\u3300-\u33ff]|[\ufe30-\ufe4f]|[\uf900-\ufaff]|[\u{2f800}-\u{2fa1f}]/u;
-      return (REGEX_CHINESE.test(str));
-    },
   },
   computed: {
     ...mapGetters(['winWidth', 'mediaHash']),
