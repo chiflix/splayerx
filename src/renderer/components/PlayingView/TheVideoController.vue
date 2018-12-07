74425727a29c1f5521e7a8059807dcb72734c697
Yvon Yan <yanyifeng@xindong.com>
2018-12-07 15:15:51 +0800

    fix(ui): fix recentPlayList abnornal disappearance

--- src/renderer/components/PlayingView/TheVideoController.vue
+++ src/renderer/components/PlayingView/TheVideoController.vue
@@ -15,6 +15,7 @@
     <recent-playlist class="recent-playlist"
     :displayState="displayState['recent-playlist']"
     :mousemove="eventInfo.get('mousemove')"
+    :isDragging.sync="isDragging"
     v-bind.sync="widgetsStatus['recent-playlist']"
     @update:playlistcontrol-showattached="updatePlaylistShowAttached"/>
     <div class="masking" v-hidden="displayState['the-progress-bar']"></div>
