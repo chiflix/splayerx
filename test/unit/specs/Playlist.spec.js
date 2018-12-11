e8f60974edab89ca362317a9eed9e81864adcd0c
YuRiJin <875830716@qq.com>
2018-12-11 19:15:47 +0800

    fix(test/playlist): delete stale test code

--- test/unit/specs/Playlist.spec.js
+++ test/unit/specs/Playlist.spec.js
@@ -16,18 +16,6 @@ describe('Playlist.vue', () => {
     expect(wrapper.vm.disY).equal('');
     expect(wrapper.vm.recentFileDel).equal(false);
     expect(wrapper.vm.showingPopupDialog).equal(false);
-    expect(wrapper.vm.move).equal(0);
-    expect(wrapper.vm.moveItem).equal(0);
-  });
-  it('showItemNum watcher works fine', () => {
-    const wrapper = mount(Playlist);
-    wrapper.setProps({ showItemNum: 5 });
-    wrapper.setProps({ showItemNum: 10 });
-    expect(wrapper.vm.move).equal(0);
-    wrapper.setData({ moveItem: -2 });
-    wrapper.setProps({ showItemNum: -1 });
-    wrapper.setProps({ showItemNum: 0 });
-    expect(wrapper.vm.moveItem).equal(-1);
   });
   it('open method works fine', () => {
     const wrapper = mount(Playlist);
@@ -35,20 +23,6 @@ describe('Playlist.vue', () => {
     wrapper.vm.open('');
     expect(wrapper.vm.showingPopupDialog).equal(true);
     wrapper.vm.open('');
-  });
-  it('openOrMove method works fine', () => {
-    const wrapper = mount(Playlist, {
-      attachToDocument: true,
-    });
-    wrapper.setData({ showingPopupDialog: false });
-    wrapper.setData({ moveItem: 0 });
-    wrapper.find('.button').trigger('click');
-    expect(wrapper.vm.showingPopupDialog).equal(true);
-    wrapper.setData({ moveItem: -1 });
-    wrapper.vm.openOrMove();
-    expect(wrapper.vm.moveItem).equal(0);
-    wrapper.setProps({ windowWidth: 1920 });
-    wrapper.vm.openOrMove();
   });
   it('backgroundUrl method works fine', () => {
     const wrapper = mount(Playlist);
@@ -65,43 +39,5 @@ describe('Playlist.vue', () => {
     const link = 'link';
     wrapper.vm.itemShortcut(link);
     expect(wrapper.vm.itemShortcut(link)).equal(`url("${link}")`);
-  });
-  it('onRecentItemClick method works fine', () => {
-    const wrapper = mount(Playlist, {
-      attachToDocument: true,
-    });
-    wrapper.setData({
-      isDragging: false, moveItem: 0,
-    });
-    wrapper.setProps({
-      isFullScreen: false, showItemNum: 5,
-    });
-    const item = [{ path: 'file:////Users/tanyang/Desktop/test.mp4' }];
-    wrapper.setProps({ lastPlayedFile: item });
-    wrapper.vm.onRecentItemClick(item, 4);
-    expect(wrapper.vm.moveItem).equal(3);
-    wrapper.setData({ moveItem: -4 });
-    wrapper.vm.onRecentItemClick(item, 2);
-    expect(wrapper.vm.moveItem).equal(0);
-    wrapper.setProps({ isFullScreen: true });
-  });
-  it('onRecentItemMousedown method works fine', () => {
-    const wrapper = mount(Playlist, {
-      attachToDocument: true,
-    });
-    wrapper.setData({
-      moveItem: -1, isDragging: true, mouseDown: true,
-    });
-    wrapper.setProps({ showItemNum: 7 });
-    wrapper.setProps({ lastPlayedFile: [{ path: 'file:////Users/tanyang/Desktop/test.mp4' }] });
-    const e = new window.Event('mousedown');
-    wrapper.vm.onRecentItemMousedown(e, 0);
-    expect(wrapper.vm.isDragging).equal(false);
-    wrapper.find('.item').trigger('mousemove');
-    expect(wrapper.vm.recentFileDel).equal(false);
-    wrapper.find('.item').trigger('mouseup');
-    wrapper.setData({ recentFileDel: true });
-    wrapper.find('.item').trigger('mouseup');
-    expect(wrapper.vm.mouseDown).equal(false);
   });
 });
