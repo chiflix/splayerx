e4189b02b4083bc55c82f1d1125c2736cdffb0c6
Tan Yang <1029862865@qq.com>
2018-12-13 14:50:42 +0800

    style(subtitle, bubble): fix subtitle style and bubble position

--- src/renderer/components/Subtitle/CueRenderer.vue
+++ src/renderer/components/Subtitle/CueRenderer.vue
@@ -8,7 +8,6 @@
 </template>
 
 <script>
-import asyncStorage from '@/helpers/asyncStorage';
 import { mapGetters } from 'vuex';
 export default {
   name: 'CueRender',
@@ -43,13 +42,6 @@ export default {
       }
       return tmp;
     },
-  },
-  created() {
-    asyncStorage.get('subtitle-style').then((data) => {
-      if (data.chosenStyle) {
-        this.$store.dispatch('updateChosenStyle', data.chosenStyle);
-      }
-    });
   },
 };
 </script>
