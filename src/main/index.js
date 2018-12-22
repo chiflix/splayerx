8dfd9452b5e8cbed20fb0afd0c17067beb1e36eb
tomasen <tomasen@gmail.com>
2018-12-22 23:50:32 +0800

    feat(mac): quit app on window-all-closed

--- src/main/index.js
+++ src/main/index.js
@@ -309,9 +309,7 @@ app.on('ready', () => {
 });
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
 
 app.on('activate', () => {
