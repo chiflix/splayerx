5a4ba310e8f78bdb40b48d45e4999fd96a08b88e
Yvon Yan <yanyifeng@xindong.com>
2019-01-02 15:19:09 +0800

    refactor(subtitle): add custom error class and types for Subtitle module

---
+++ src/renderer/components/Subtitle/SubtitleLoader/errors.js
@@ -0,0 +1,21 @@
+export class SubtitleError extends Error {
+  constructor(code, message) {
+    // https://nodejs.org/api/errors.html
+    // https://gist.github.com/slavafomin/b164e3e710a6fc9352c934b9073e7216
+    // https://github.com/nodejs/node/blob/master/lib/internal/errors.js
+    super(message);
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+    this.code = code;
+  }
+}
+
+export const ErrorCodes = {
+  SUBTITLE_NO_PERMISSIONS: 'ERR_SUBTITLE_NO_PERMISSIONS',
+  SUBTITLE_NO_SUCH_FILE: 'ERR_SUBTITLE_NO_SUCH_FILE',
+  SUBTITLE_CURRENTLY_USING: 'ERR_SUBTITLE_CURRENTLY_USING',
+  SUBTITLE_RETRIEVE_FAILED: 'ERR_SUBTITLE_RETRIEVE_TIMEOUT',
+  SUBTITLE_INVALID_TYPE: 'ERR_SUBTITLE_INVALID_TYPE',
+  SUBTITLE_INVALID_FORMAT: 'ERR_SUBTITLE_INVALID_FORMAT',
+  SUBTITLE_INVALID_CONTENT: 'ERR_SUBTITLE_INVALID_CONTENT',
+};
