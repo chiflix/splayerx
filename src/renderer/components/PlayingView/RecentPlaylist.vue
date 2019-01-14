639e3dda34c2c4b7b3db027e346806dbb0a8c669
Tan Yang <1029862865@qq.com>
2019-01-14 17:49:21 +0800

    fix(menu): fix menu can not closed

--- src/renderer/components/PlayingView/RecentPlaylist.vue
+++ src/renderer/components/PlayingView/RecentPlaylist.vue
@@ -215,6 +215,7 @@ export default {
       if (this.mousedownCurrentTarget !== 'notification-bubble') {
         if (this.lastDragging) {
           this.clearMousedown({ target: '' });
+          this.$emit('update:lastDragging', false);
         } else if (val !== this.$options.name && this.backgroundDisplayState) {
           this.$emit('update:playlistcontrol-showattached', false);
         }
