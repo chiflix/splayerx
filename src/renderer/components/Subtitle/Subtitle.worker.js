f04aaa6cd022d872f8d1c57a4529813e6e628d92
Yvon Yan <yanyifeng@xindong.com>
2019-01-23 13:17:50 +0800

    chore(package): remove useless dependencies for subtitle module

--- src/renderer/components/Subtitle/Subtitle.worker.js
+++
@@ -1,6 +0,0 @@
-import iso6393 from 'iso-639-3';
-
-/* eslint-disable import/prefer-default-export */
-export function findISO6393Locale(identifier) {
-  return (iso6393.filter(lang => lang.iso6393 === identifier))[0];
-}
