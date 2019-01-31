be0ad1c398b57f39668115b68636ea502282cebe
Yvon Yan <yanyifeng@xindong.com>
2019-01-31 10:38:21 +0800

    refactor(vuex): remove logger for vuex mutations

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import createLogger from 'vuex/dist/logger';
 
 import modules from './modules';
 
@@ -9,8 +8,5 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   modules,
   strict: process.env.NODE_ENV !== 'production',
-  plugins: process.env.NODE_ENV === 'development' ? [createLogger({
-    filter: mutation => mutation.type === 'AUDIO_TRACK_LIST_UPDATE',
-  })] : [],
 });
 
