7cbdb108957442cac5017d11b0445e4c46b617d1
YvonTre <yanyifeng@xindong.com>
2019-02-16 15:17:23 +0800

    build(idb): upgrade idb version to 3.0.2

--- src/renderer/helpers/infoDB.js
+++ src/renderer/helpers/infoDB.js
@@ -1,12 +1,10 @@
-
-import idb from 'idb';
+import { openDb } from 'idb';
 import { INFO_DATABASE_NAME, INFO_SCHEMA, INFODB_VERSION } from '@/constants';
 import addLog from './index';
 
-
 /**
-* You can change schema info in 'constants.js'
-*/
+ * You can change schema info in 'constants.js'
+ */
 export default class InfoDB {
   #db;
 
@@ -16,7 +14,7 @@ export default class InfoDB {
    */
   async getDB() {
     if (this.db) return this.db;
-    this.db = await idb.open(INFO_DATABASE_NAME, INFODB_VERSION, (upgradeDB) => {
+    this.db = await openDb(INFO_DATABASE_NAME, INFODB_VERSION, (upgradeDB) => {
       INFO_SCHEMA.forEach((schema) => {
         let store;
         if (!upgradeDB.objectStoreNames.contains(schema.name)) {
@@ -83,10 +81,12 @@ export default class InfoDB {
     const db = await this.getDB();
     const tx = db.transaction('recent-played');
     let val;
-    tx.objectStore('recent-played').index('lastOpened').iterateCursor(null, 'prev', (cursor) => {
-      if (!cursor) return;
-      val = cursor.value;
-    });
+    tx.objectStore('recent-played')
+      .index('lastOpened')
+      .iterateCursor(null, 'prev', (cursor) => {
+        if (!cursor) return;
+        val = cursor.value;
+      });
     return tx.complete.then(() => Promise.resolve(val));
   }
 
@@ -104,11 +104,13 @@ export default class InfoDB {
     const db = await this.getDB();
     const tx = db.transaction(schema);
     const res = [];
-    tx.objectStore(schema).index(key).iterateCursor(null, direction, (cursor) => {
-      if (!cursor) return;
-      res.push(cursor.value);
-      cursor.continue();
-    });
+    tx.objectStore(schema)
+      .index(key)
+      .iterateCursor(null, direction, (cursor) => {
+        if (!cursor) return;
+        res.push(cursor.value);
+        cursor.continue();
+      });
     return tx.complete.then(() => Promise.resolve(res));
   }
 
@@ -122,11 +124,18 @@ export default class InfoDB {
   async get(schema, key, val) {
     const db = await this.getDB();
     if (val) {
-      const value = await db.transaction(schema).objectStore(schema).index(key).get(val);
+      const value = await db
+        .transaction(schema)
+        .objectStore(schema)
+        .index(key)
+        .get(val);
       return value;
     }
     val = key;
-    const value = await db.transaction(schema).objectStore(schema).get(val);
+    const value = await db
+      .transaction(schema)
+      .objectStore(schema)
+      .get(val);
     return value;
   }
 
@@ -148,4 +157,3 @@ export default class InfoDB {
     return val;
   }
 }
-
