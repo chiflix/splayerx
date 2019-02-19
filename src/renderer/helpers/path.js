2814a713b752f40bd957c66ee6512fc12ad0e9bf
Harry Tang <harry@Harrys-MacBook-Pro.local>
2019-02-19 11:50:32 +0800

    refactor(parseName): use reg replace third libs to parseName

--- src/renderer/helpers/path.js
+++ src/renderer/helpers/path.js
@@ -31,7 +31,89 @@ export function fileUrlToPath(fileUrl) {
   return filePath;
 }
 
+// season math reg
+const SEREG = /([s|e](\d+)|(\d+)季|([零|一|二|三|四|五|六|七|八|九|十|百|千]+)季)/i;
+// episode match reg
+const EPREG = /([e|p](\d+)|(\d+)集|([零|一|二|三|四|五|六|七|八|九|十|百|千]+)集)/i;
+// 分位数列表
+const QUANTILES = ['千', '百', '十'];
+// 中文数字匹配键值对
+const ZHNMAP = {
+  零: 0,
+  一: 1,
+  二: 2,
+  三: 3,
+  四: 4,
+  五: 5,
+  六: 6,
+  七: 7,
+  八: 8,
+  九: 9,
+};
+
+/**
+ * 中文数字转换成阿拉伯数字，千分位以内
+ * @description
+ * @author tanghaixiang@xindong.com
+ * @date 2019-02-19
+ * @param {String} str 中文数字 example: '一千九百二十五'
+ * @returns
+ */
+function zh2Numer(str) {
+  if (Object.prototype.toString.call(str).toLowerCase() !== '[object string]') throw new Error('str should be String');
+  let num = 0;
+  const len = QUANTILES.length;
+  QUANTILES.forEach((e, i) => {
+    const tmpList = str.split(e);
+    if (tmpList.length === 1) return;
+    str = tmpList[1];
+    if (ZHNMAP[tmpList[0]] !== undefined) {
+      num += ZHNMAP[tmpList[0]] * (10 ** (len - i));
+    }
+  });
+  if (ZHNMAP[str] !== undefined) {
+    num += ZHNMAP[str];
+  }
+  return num;
+}
+
+/**
+ * 匹配路径中视频文件名称里面的season和episode
+ * @description
+ * @author tanghaixiang@xindong.com
+ * @date 2019-02-19
+ * @export
+ * @param {String} path 视频名称
+ * @returns {Object} example: {season: null, episode: "02"}
+ */
+export function parseNameFromPath(path) {
+  if (Object.prototype.toString.call(path).toLowerCase() !== '[object string]') throw new Error('path should be String');
+  const ls = [null, null]; // [se, ep]
+  const l = [SEREG, EPREG];
+  l.forEach((r, i) => {
+    path = path.trim().replace(r, (...a) => {
+      if (a[2] || a[3]) { // match 第一个和第二个子项(\d+)
+        const p = a[2] !== undefined ? parseInt(a[2], 10) : parseInt(a[3], 10);
+        ls[i] = p < 10 ? `0${p}` : `${p}`;
+        return '';
+      }
+      if (a[4]) { // match 第三个子项 中文字符
+        const p = zh2Numer(a[4]);
+        if (p > 0) ls[i] = p < 10 ? `0${p}` : `${p}`;
+        return '';
+      }
+      if (a[0]) return a[0];
+      return '';
+    });
+  });
+  return {
+    season: ls[0],
+    episode: ls[1],
+  };
+}
+
 export default {
   filePathToUrl,
   fileUrlToPath,
+  parseNameFromPath,
 };
--- test/unit/specs/helpers/path.spec.js
+++ test/unit/specs/helpers/path.spec.js
@@ -1,4 +1,4 @@
-import { filePathToUrl, fileUrlToPath } from '../../../../src/renderer/helpers/path';
+import { filePathToUrl, fileUrlToPath, parseNameFromPath } from '../../../../src/renderer/helpers/path';
 
 describe('helper.path', () => {
   it('should convert file path to url correctly', () => {
@@ -17,5 +17,19 @@ describe('helper.path', () => {
     expect(fileUrlToPath('file:///C:/Users/test/%E6%B5%8B%E8%AF%95.mp4?a=1&b=2#c')).to.be.equal('C:\\Users\\test\\测试.mp4');
 
     Object.defineProperty(process, 'platform', { value: currentPlatform });
+  });
+  it('should parse name correctly', () => {
+    ['S01E01', 'S1E1', 'SE01EP01', 'SE1EP1', '第一季第一集', '第1季第1集', '第01季第01集'].forEach((e) => {
+      expect(JSON.stringify(parseNameFromPath(e))).to.be.equals(JSON.stringify({
+        season: '01',
+        episode: '01',
+      }));
+    });
+    ['EP01', '第一集', '第1集', ' 第01集', '01集'].forEach((e) => {
+      expect(JSON.stringify(parseNameFromPath(e))).to.be.equals(JSON.stringify({
+        season: null,
+        episode: '01',
+      }));
+    });
   });
 });
