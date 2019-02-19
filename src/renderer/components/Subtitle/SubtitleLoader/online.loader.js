eea01baec5e667da5a860bcc16a2d448d6e77a7f
Tan Yang <1029862865@qq.com>
2019-02-19 14:45:52 +0800

    fix(subtitle): fix subtitle normalizer

--- src/renderer/components/Subtitle/SubtitleLoader/online.loader.js
+++ src/renderer/components/Subtitle/SubtitleLoader/online.loader.js
@@ -1,15 +1,21 @@
 import { tagsGetter, loadOnlineTranscript, normalizeCode } from './utils';
 
 const baseTags = { alignment: 2, pos: null };
-const normalizer = parsedSubtitle => parsedSubtitle.map(({ startTime, endTime, text }) => ({
-  info: {},
-  dialogues: {
-    start: startTime,
-    end: endTime,
-    text: text.replace(/\{[^{}]*\}/g, ''),
-    tags: tagsGetter(text, baseTags),
-  },
-}));
+const normalizer = (parsedSubtitle) => {
+  const finalDialogues = [];
+  parsedSubtitle.forEach(({ startTime, endTime, text }) => {
+    finalDialogues.push({
+      start: startTime,
+      end: endTime,
+      text: text.replace(/\{[^{}]*\}/g, ''),
+      tags: tagsGetter(text, baseTags),
+    });
+  });
+  return {
+    info: {},
+    dialogues: finalDialogues,
+  };
+};
 
 export default {
   longName: 'Online Transcript',
