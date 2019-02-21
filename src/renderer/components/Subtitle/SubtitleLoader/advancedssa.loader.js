f344da8db6ea30d4cc841378ca9acc0a1e639e10
YvonTre <yanyifeng@xindong.com>
2019-02-21 11:57:33 +0800

    style(eslint): fix lint errors

--- src/renderer/components/Subtitle/SubtitleLoader/advancedssa.loader.js
+++ src/renderer/components/Subtitle/SubtitleLoader/advancedssa.loader.js
@@ -2,7 +2,7 @@ import flow from 'lodash/flow';
 import pick from 'lodash/pick';
 import { compile } from 'ass-compiler';
 
-import { localLanguageLoader, localNameLoader, loadLocalFile, localIdLoader } from './utils';
+import { localLanguageLoader, localNameLoader, loadLocalFile } from './utils';
 
 const baseInfo = {
   // Title: '',
