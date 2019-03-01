87ad649bcf5f5af6e918deae0313f5d03d927f83
Tan Yang <1029862865@qq.com>
2019-03-01 17:09:12 +0800

    fix(subtitle): fix can not load ass tags

--- src/renderer/components/Subtitle/SubtitleRenderer.vue
+++ src/renderer/components/Subtitle/SubtitleRenderer.vue
@@ -185,11 +185,15 @@ export default {
         const currentCues = [];
         cues.forEach((item) => {
           let currentText = '';
+          let currentTags = {};
           item.fragments.forEach((cue) => {
             currentText += cue.text;
+            if (cue.tags) {
+              currentTags = cue.tags;
+            }
           });
           currentCues.push({
-            start: item.start, end: item.end, tags: item.fragments[0].tags, text: currentText,
+            start: item.start, end: item.end, tags: currentTags, text: currentText,
           });
         });
         return currentCues;
