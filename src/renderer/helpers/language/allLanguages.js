417d2aff744004c6dd7363ee211c6c8d20462a92
Tan Yang <1029862865@qq.com>
2019-03-06 16:45:13 +0800

    feat(subtitle): support more subtitle languges to choose

--- src/renderer/helpers/language/allLanguages.js
+++ src/renderer/helpers/language/allLanguages.js
@@ -17,9 +17,51 @@ export const codes = {
   ],
   ja: [
     'ja',
+    'jpn',
   ],
   ar: [
     'ar',
+    'ara',
+    'arb',
+  ],
+  pt: [
+    'pt',
+    'por',
+  ],
+  ko: [
+    'ko',
+    'kor',
+  ],
+  es: [
+    'es',
+    'spa',
+  ],
+  fr: [
+    'fr',
+    'fre',
+    'fra',
+  ],
+  de: [
+    'de',
+    'ger',
+    'deu',
+  ],
+  ru: [
+    'ru',
+    'rus',
+  ],
+  cs: [
+    'cs',
+    'cze',
+    'ces',
+  ],
+  it: [
+    'it',
+    'ita',
+  ],
+  hi: [
+    'hi',
+    'hin',
   ],
 };
 
@@ -28,5 +70,14 @@ export const names = {
   'zh-TW': '繁體中文',
   en: 'English',
   ja: '日本語',
-  ar: '',
+  ar: 'العربية',
+  pt: 'Português',
+  ko: '한국어',
+  es: 'Español',
+  fr: 'Français',
+  de: 'Deutsch',
+  ru: 'Русский',
+  cs: 'čeština',
+  it: 'Italiano',
+  hi: 'हिन्दी',
 };
