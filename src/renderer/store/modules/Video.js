4939795134dd5bb7ff6d54344bde64d9a573e0bd
YuRiJin <875830716@qq.com>
2019-03-06 15:38:29 +0800

    featvolume): decrease volumen increment with keyboard

--- src/renderer/store/modules/Video.js
+++ src/renderer/store/modules/Video.js
@@ -214,13 +214,13 @@ const actions = {
   },
   [videoActions.INCREASE_VOLUME]({ dispatch, commit, state }, delta) {
     if (state.muted) dispatch(videoActions.TOGGLE_MUTED);
-    const finalDelta = delta || 10;
+    const finalDelta = delta || 5;
     const finalVolume = state.volume + finalDelta;
     commit(videoMutations.VOLUME_UPDATE, finalVolume > 100 ? 100 : finalVolume);
   },
   [videoActions.DECREASE_VOLUME]({ dispatch, commit, state }, delta) {
     if (state.muted) dispatch(videoActions.TOGGLE_MUTED);
-    const finalDelta = delta || 10;
+    const finalDelta = delta || 5;
     const finalVolume = state.volume - finalDelta;
     commit(videoMutations.VOLUME_UPDATE, finalVolume < 0 ? 0 : finalVolume);
     if (finalVolume <= 0) commit(videoMutations.MUTED_UPDATE, true);
