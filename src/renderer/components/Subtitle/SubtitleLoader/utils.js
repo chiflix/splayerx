542d1fa80748d50bda527c3c91222f5e05490693
Yvon Yan <yanyifeng@xindong.com>
2019-03-07 15:06:33 +0800

    fix(subtitle): tempoary set pos to 4096*30 for utf16le encoded subtitles

--- src/renderer/components/Subtitle/SubtitleLoader/utils.js
+++ src/renderer/components/Subtitle/SubtitleLoader/utils.js
@@ -39,7 +39,7 @@ function getFragmentBuffer(path, detectEncoding) {
   return new Promise((resolve, reject) => {
     open(path, 'r', (err, fd) => {
       if (err) reject(err);
-      const pos = detectEncoding ? 0 : Math.round(statSync(path).size / 2);
+      const pos = detectEncoding ? 0 : 4096 * 30;
       const buf = Buffer.alloc(4096); // https://github.com/Microsoft/vscode/blob/f886dd4fb84bb82478bfab4a68cd3f31b32f5eb5/src/vs/base/node/encoding.ts#L268
       readSync(fd, buf, 0, 4096, pos);
       resolve(buf);
