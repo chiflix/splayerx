52ccd3e6907a4755b7814a2f9f82d7a1ee686a47
Yan Yifeng <yanyifeng@xindong.com>
2019-04-18 15:39:52 +0800

    feat(trackpad): add trackpad and wheel listener

--- src/shared/trackpadSupport/renderer.js
+++ src/shared/trackpadSupport/renderer.js
@@ -8,13 +8,16 @@ class TrackpadWheelManager {
   }
   get duration() { return this._duration; }
 
+  isTrackpadBegan = false;
+  isTrackpadEnd = true;
+
+  direction = '';
+
   install(Vue) {
     const self = this;
     Vue.mixin({
       methods: {
         registerDurationWatcher() {
-          // console.log('TrackPad: hahaha.');
-          // console.log(this.$store.state.Video.duration);
           this.unregisterDurationWatcher = this.$watch(
             () => this.$store.state.Video.duration,
             (newVal) => { self.duration = newVal; },
@@ -22,7 +25,32 @@ class TrackpadWheelManager {
         },
       },
       created() {
-        if (this === this.$root) this.registerDurationWatcher();
+        if (this === this.$root) {
+          this.registerDurationWatcher();
+          this?.$electron.ipcRenderer.on('scroll-touch-begin', () => {
+            self.isTrackpadBegan = true;
+            self.isTrackpadEnd = false;
+          });
+          this?.$electron.ipcRenderer.on('scroll-touch-end', () => {
+            self.isTrackpadBegan = false;
+            self.isTrackpadEnd = true;
+            self.direction = '';
+          });
+
+          // eslint-disable-next-line complexity
+          document.addEventListener('wheel', (event) => {
+            const { ctrlKey } = event;
+            const { isTrackpadBegan, direction } = self;
+            if (!ctrlKey && isTrackpadBegan) {
+              const { deltaX, deltaY } = event;
+              if (deltaX && (!direction || direction === 'left' || direction === 'right')) {
+                self.direction = deltaX > 0 ? 'left' : 'right';
+              } else if (deltaY && (!direction || direction === 'up' || direction === 'down')) {
+                self.direction = deltaY > 0 ? 'up' : 'down';
+              }
+            }
+          });
+        }
       },
       destoryed() {
         if (this === this.$root && typeof this.unregisterDurationWatcher === 'function') {
