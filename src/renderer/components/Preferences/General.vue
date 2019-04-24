cd7adfe8824dcb22e0421040001bd4b56ea6b67b
YuRiJin <875830716@qq.com>
2019-04-24 17:42:04 +0800

    fix(general): no content

--- src/renderer/components/Preferences/General.vue
+++ src/renderer/components/Preferences/General.vue
@@ -88,6 +88,7 @@ export default {
     };
   },
   created() {
+    electron.ipcRenderer.send('get-restore-state');
     electron.ipcRenderer.on('restore-state', (event, state) => {
       this.restoreContent = state ? this.$t('preferences.general.relaunch')
         : this.$t('preferences.general.setButton');
