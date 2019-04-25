8df5b0b74e5b3446ebb2890b8fb3a4460a4ab32e
Yan Yifeng <yanyifeng@xindong.com>
2019-04-25 13:52:16 +0800

    refactor(vue): rearrange vue plugin's installment

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -30,6 +30,7 @@ import asyncStorage from '@/helpers/asyncStorage';
 import { videodata } from '@/store/video';
 import NotificationBubble, { addBubble } from '../shared/notificationControl';
 import { SNAPSHOT_FAILED, SNAPSHOT_SUCCESS } from '../shared/notificationcodes';
+import InputPlugin from '@/plugins/input';
 
 // causing callbacks-registry.js 404 error. disable temporarily
 // require('source-map-support').install();
@@ -83,7 +84,6 @@ Vue.use(VueI18n);
 Vue.use(VueElectronJSONStorage);
 Vue.use(VueResource);
 Vue.use(AsyncComputed);
-
 Vue.use(VueAnalytics, {
   id: (process.env.NODE_ENV === 'production') ? 'UA-2468227-6' : 'UA-2468227-5',
   router,
@@ -95,17 +95,20 @@ Vue.use(VueAnalytics, {
   ],
 });
 
-Vue.mixin(helpers);
-
-hookVue(Vue);
-
-Vue.prototype.$bus = new Vue(); // Global event bus
-
+// Custom plugin area
+Vue.use(InputPlugin);
+// i18n and its plugin
 const i18n = new VueI18n({
   locale: getSystemLocale(), // set locale
   messages, // set locale messages
 });
 Vue.use(NotificationBubble, i18n);
+
+Vue.mixin(helpers);
+
+hookVue(Vue);
+
+Vue.prototype.$bus = new Vue(); // Global event bus
 
 /* eslint-disable no-new */
 new Vue({
