9e96f2aee96a71924ea04a9e1d6ef7ebf4c7ddf6
Max <ymmuse@gmail.com>
2019-05-07 13:53:52 +0900

    fix(sentry-sdk) get rid of debug code

--- src/shared/sentry.js
+++ src/shared/sentry.js
@@ -4,7 +4,7 @@ import * as Sentry from '@sentry/electron';
 import * as Integrations from '@sentry/integrations';
 
 
-if (1 || process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV !== 'development') {
   Sentry.init({
     dsn: 'https://6a94feb674b54686a6d88d7278727b7c@sentry.io/1449341',
     enableNative: true,
