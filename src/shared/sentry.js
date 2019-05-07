22b5f304a5965ccb9696364e632968a91cb1c374
Max <ymmuse@gmail.com>
2019-05-07 13:50:55 +0900

    add settting vue on sentry

--- src/shared/sentry.js
+++ src/shared/sentry.js
@@ -1,10 +1,19 @@
 // Be sure to call Sentry function as early as possible in the renderer process
+import Vue from 'vue';
 import * as Sentry from '@sentry/electron';
+import * as Integrations from '@sentry/integrations';
 
-if (process.env.NODE_ENV !== 'development') {
+
+if (1 || process.env.NODE_ENV !== 'development') {
   Sentry.init({
     dsn: 'https://6a94feb674b54686a6d88d7278727b7c@sentry.io/1449341',
     enableNative: true,
+    integrations: [
+      new Integrations.Vue({
+        Vue,
+        attachProps: true,
+      }),
+    ],
   });
 }
 
