aadea6aee31a2a37ef9dfc901e279266c8f24ff7
YuRiJin <875830716@qq.com>
2019-06-05 21:56:00 +0800

    doc(interface): add doc on IDB

--- src/renderer/interfaces/services/IDB.ts
+++ src/renderer/interfaces/services/IDB.ts
@@ -1,4 +1,3 @@
-import { clear } from "winston";
 
 export type RawPlaylistItem = {
   items: IDBValidKey[],
@@ -35,12 +34,70 @@ export type SubtitleItem = {
   type: string,
 }
 export interface IDB {
+  /**
+   * @param  {string} database
+   * @param  {string} schema
+   * @param  {RawMediaItem|RawPlaylistItem} data
+   * @returns {Promise<IDBValidKey>}
+   * 向 database -> schema 中添加data，返回key值
+   */
   add(database: string, schema: string, data: RawMediaItem | RawPlaylistItem): Promise<number>
+  /**
+   * @param  {string} database
+   * @param  {string} schema
+   * @param  {number} key
+   * @param  {PlaylistItem|MediaItem} data
+   * @returns {Promise<IDBValidKey>}
+   * 向 database -> schema 中更新主键为key的数据，返回key值
+   */
   update(database: string, schema: string, key: number, data: PlaylistItem | MediaItem): Promise<number>
+  /**
+   * @param  {string} database
+   * @param  {string} schema
+   * @param  {number} key
+   * @returns {Promise<undefined>}
+   * 删除 database -> schema 中主键为key的记录
+   */
   delete(database:string, schema: string, key: number): Promise<undefined>
+  /**
+   * @param  {string} database
+   * @param  {string} schema
+   * @returns {Promise<undefined>}
+   * 清除 database -> schema 中的所有记录
+   */
   clear(database: string, schema: string): Promise<undefined>
+  /**
+   * @param  {string} database
+   * @param  {string} schema
+   * @param  {IDBKeyRange} keyRange
+   * @returns {Promise<PlaylistItem[] | MediaItem[] | SubtitleItem[]>}
+   * 返回 database -> schema 中所有记录
+   */
   getAll(database: string, schema: string, keyRange: IDBKeyRange): Promise<PlaylistItem[] | MediaItem[] | SubtitleItem[]>
+  /**
+   * @param  {string} database
+   * @param  {string} schema
+   * @param  {number} key
+   * @returns {Promise<PlaylistItem | MediaItem | SubtitleItem>}
+   * 返回 database -> schema 中主键为key的记录
+   */
   getValueByKey(database: string, schema: string, key: number): Promise<PlaylistItem | MediaItem | SubtitleItem | undefined>
+  /**
+   * @param  {string} database
+   * @param  {string} schema
+   * @param  {string} index
+   * @param  {string|number} value
+   * @returns {Promise<PlaylistItem | MediaItem | SubtitleItem>}
+   * 返回 database -> schema 中属性index的值为value的第一条记录
+   */
   getValueByIndex(database: string, schema: string, index: string, value: string | number): Promise<PlaylistItem | MediaItem | SubtitleItem | undefined>
+  /**
+   * @param  {string} database
+   * @param  {string} schema
+   * @param  {string} index
+   * @param  {string|number} value
+   * @returns {Promise<PlaylistItem[] | MediaItem[] | SubtitleItem[]>}
+   * 返回 database -> schema 中属性index为value的所有记录
+   */
   getAllValueByIndex(database: string, schema: string, index: string, value: string | number): Promise<PlaylistItem[] | MediaItem[] | SubtitleItem[]>
 }
