de60cb646d80f1c0353f2aff894a853f8b649495
Yan Yifeng <yanyifeng@xindong.com>
2019-06-12 15:48:42 +0800

    fix(language): typescripten index.ts

--- src/renderer/libs/language/index.ts
+++ src/renderer/libs/language/index.ts
@@ -1,15 +1,15 @@
-import { names, codes } from './allLanguages';
+import { LanguageNames, codes } from './allLanguages';
 
-export function normalizeCode(code) {
+export function normalizeCode(code: string) {
   const result = Object.keys(codes).find(name => codes[name].includes(code));
   return result || '';
 }
 
-export function codeToLanguageName(code) {
+export function codeToLanguageName(code: string) {
   const standardCode = normalizeCode(code);
-  return standardCode === '' ? '' : names[standardCode];
+  return standardCode === '' ? '' : LanguageNames[standardCode];
 }
 
-export function codeIndex(code) {
+export function codeIndex(code: string) {
   return Object.keys(codes).indexOf(normalizeCode(code));
 }
