af60822afd2ccf9b4a32165384b98ab30695a64e
Yan Yifeng <yanyifeng@xindong.com>
2019-06-12 20:58:58 +0800

    refactor(searchers): typescripten retrieveEmbeddedList
    1. Add class declaration for local, online and embedded.
    2. TypeScripten retrieveEmbeddedList.
    3. Add interface for SubtitleStream.

--- src/renderer/interfaces/services/ISubtitle.ts
+++ src/renderer/interfaces/services/ISubtitle.ts
@@ -1,4 +1,14 @@
 export const subtitleExtensions = ['ass', 'srt', 'ssa', 'vtt'];
+export enum SubtitleCodec {
+  AdvancedSubStationAplha = 'ASS (Advanced SSA) subtitle',
+  SubRip = 'SubRip subtitle',
+  WebVTT = 'WebVTT subtitle',
+};
+export const subtitleCodecs = [
+  'ASS (Advanced SSA) subtitle', 'ass',
+  'SubRip subtitle', 'srt',
+  'WebVTT subtitle', 'vtt',
+];
 export enum SubtitleFormat {
   AdvancedSubStationAplha = 'ass',
   SubRip = 'srt',
@@ -12,11 +22,53 @@ export enum SubtitleType {
   Local,
   Modified,
 }
+export interface ISubtitleStream {
+  codec_type: string;
+  codec_name: string;
+  index: number;
+  tags: {
+    language: LanguageNames;
+    title: string;
+  };
+  disposition: {
+    default: 0 | 1;
+  };
+}
+interface IEmbeddedSubtitleOrigin {
+  streamIndex: number;
+  videoSrc: string;
+}
 export interface IRawSubtitle {
-  origin: string;
+  origin: string | IEmbeddedSubtitleOrigin;
   format: SubtitleFormat;
   type: SubtitleType;
   language?: LanguageNames;
-  ranking?: number;
+}
+
+export declare class RawLocalSubtitle implements IRawSubtitle {
+  origin: string;
+  format: SubtitleFormat;
+  type: SubtitleType.Local;
+  language?: LanguageNames;
+  name: string;
+}
+export declare class RawOnlineSubtitle implements IRawSubtitle {
+  origin: string;
+  format: SubtitleFormat.Online;
+  type: SubtitleType.Online;
+  language: LanguageNames;
+  ranking: number;
+}
+export declare class RawEmbeddedSubtitle implements IRawSubtitle {
+  origin: {
+    streamIndex: number;
+    videoSrc: string;
+  };
+  format: SubtitleFormat;
+  type: SubtitleType.Embedded;
+  language?: LanguageNames;
+  codec: string;
+  isDefault: boolean;
+  name?: string;
 }
 import { LanguageNames } from '@/libs/language/allLanguages';
--- src/renderer/services/subtitle/searchers.ts
+++ src/renderer/services/subtitle/searchers.ts
@@ -2,15 +2,19 @@ import {
   dirname, extname, basename, join,
 } from 'path';
 import { readdir } from 'fs';
-import { ipcRenderer } from 'electron';
+import { ipcRenderer, Event } from 'electron';
 import Sagi from '@/helpers/sagi';
 import helpers from '@/helpers';
-import { subtitleExtensions, IRawSubtitle, SubtitleType, SubtitleFormat } from '@/interfaces/services/ISubtitle';
+import {
+  subtitleExtensions, subtitleCodecs,
+  SubtitleType, SubtitleFormat, SubtitleCodec, ISubtitleStream,
+  RawLocalSubtitle, RawOnlineSubtitle, RawEmbeddedSubtitle,
+} from '@/interfaces/services/ISubtitle';
 import { LanguageNames } from '@/libs/language/allLanguages';
 
 const { mediaQuickHash: calculateMediaIdentity } = helpers.methods;
 
-export function searchForLocalList(videoSrc: string): Promise<IRawSubtitle[]> {
+export function searchForLocalList(videoSrc: string): Promise<RawLocalSubtitle[]> {
   return new Promise((resolve, reject) => {
     const videoDir = dirname(videoSrc);
     const videoBasename = basename(videoSrc, extname(videoSrc));
@@ -30,6 +34,7 @@ export function searchForLocalList(videoSrc: string): Promise<IRawSubtitle[]> {
           .filter(isValidSubtitle)
           .map(filename => ({
             origin: join(videoDir, filename),
+            name: filename,
             type: SubtitleType.Local,
             format: SubtitleFormat[extname(filename.slice(1))],
           }))
@@ -39,8 +44,8 @@ export function searchForLocalList(videoSrc: string): Promise<IRawSubtitle[]> {
   });
 }
 
-export function fetchOnlineList(videoSrc: string, languageCode: LanguageNames, hints: string): Promise<IRawSubtitle[]> {
-  const subtitleInfoNormalizer = (subtitle: any): IRawSubtitle => {
+export function fetchOnlineList(videoSrc: string, languageCode: LanguageNames, hints: string): Promise<RawOnlineSubtitle[]> {
+  const subtitleInfoNormalizer = (subtitle: any): RawOnlineSubtitle => {
     const { languageCode: language, transcriptIdentity: origin, ranking } = subtitle;
     return ({
       origin,
@@ -61,26 +66,28 @@ export function fetchOnlineList(videoSrc: string, languageCode: LanguageNames, h
   });
 }
 
-export function retrieveEmbeddedList(videoSrc, supportedCodecs) {
+export function retrieveEmbeddedList(videoSrc: string): Promise<RawEmbeddedSubtitle[]> {
   ipcRenderer.send('mediaInfo', videoSrc);
   return new Promise((resolve, reject) => {
     setTimeout(() => { reject(new Error('Embedded Subtitles Retrieve Timeout!')); }, 20000);
-    ipcRenderer.once(`mediaInfo-${videoSrc}-reply`, (event, info) => {
+    ipcRenderer.once(`mediaInfo-${videoSrc}-reply`, (event: Event, info: string) => {
       try {
-        const subtitleStreams = JSON.parse(info).streams
-          .filter(stream => stream.codec_type === 'subtitle' && supportedCodecs.includes(stream.codec_name)); // eslint-disable-line camelcase;
-        if (!subtitleStreams.length) resolve([]);
-        const normalizeStream = subtitleStream => ({
-          src: subtitleStream.index,
-          type: 'embedded',
-          options: {
-            videoSrc,
+        const subtitleStreams: ISubtitleStream[] = JSON.parse(info).streams
+          .filter((stream: ISubtitleStream) => (
+            stream.codec_type === 'subtitle' &&
+            subtitleCodecs.includes(stream.codec_name)
+          ));
+        const normalizeStream = (subtitleStream: ISubtitleStream): RawEmbeddedSubtitle => ({
+          origin: {
             streamIndex: subtitleStream.index,
-            codec: subtitleStream.codec_name,
-            language: subtitleStream.tags.language,
-            name: subtitleStream.tags.title,
-            isDefault: !!subtitleStream.disposition.default,
-          }, // eslint-disable-line camelcase
+            videoSrc,
+          },
+          format: SubtitleCodec[subtitleStream.codec_name],
+          type: SubtitleType.Embedded,
+          language: subtitleStream.tags.language,
+          name: subtitleStream.tags.title,
+          codec: subtitleStream.codec_name,
+          isDefault: !!subtitleStream.disposition.default,
         });
         resolve(subtitleStreams.map(normalizeStream));
       } catch (error) {
