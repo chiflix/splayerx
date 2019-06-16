ba37ff75e027cbde046739c625d75eec0513558d
YvonTre <youngfornever@outlook.com>
2019-06-16 18:04:39 +0800

    fix(upload): fix subtitle's upload service with ts

--- src/renderer/services/subtitle/upload.ts
+++ src/renderer/services/subtitle/upload.ts
@@ -1,24 +1,13 @@
 import PQueue from 'p-queue';
 import Sagi from '@/helpers/sagi';
+import { TrainingData } from 'sagi-api/training/v1/training_pb';
 
-const validProperties = [
-  'mediaIdentity',
-  'format',
-  'playedTime',
-  'totalTime',
-  'delay',
-];
-function checkProperty(object) {
-  return validProperties.every((property) => {
-    const value = object[property];
-    return value !== undefined && value !== null;
-  });
-}
+export type SubtitleUploadParameter = TrainingData.AsObject;
 
 export class TranscriptQueue {
   queue = new PQueue();
 
-  constructor(pqueueInstance) {
+  constructor(pqueueInstance: PQueue) {
     if ((pqueueInstance instanceof PQueue)) {
       this.queue = pqueueInstance;
     } else throw new TypeError('Expected PQueue instance provided.');
@@ -26,25 +15,27 @@ export class TranscriptQueue {
 
   subtitleState = {};
 
-  async add(subtitle, isManual) {
-    if (checkProperty(subtitle)) {
-      const id = `${subtitle.src}-${subtitle.mediaIdentity}`;
-      const options = { priority: 0 };
-      if (!isManual) {
-        switch (this.subtitleState[id]) {
-          default:
-            this.subtitleState[id] = 'loading';
-            break;
-          case 'failed':
-            this.subtitleState[id] = 'loading';
-            options.priority = 1;
-            break;
-          case 'loading':
-          case 'successful':
-            return false;
-        }
-      }
-      return this.queue.add(() => Sagi.pushTranscript(subtitle), options)
+  async add(subtitle: SubtitleUploadParameter) {
+    const id = `${subtitle.hints}-${subtitle.mediaIdentity}`;
+    const options = { priority: 0 };
+    switch (this.subtitleState[id]) {
+      default:
+        this.subtitleState[id] = 'loading';
+        break;
+      case 'failed':
+        this.subtitleState[id] = 'loading';
+        options.priority = 1;
+        break;
+      case 'loading':
+      case 'successful':
+        return false;
+    }
+  }
+
+  async addManually(subtitle: SubtitleUploadParameter) {
+    const id = `${subtitle.hints}-${subtitle.mediaIdentity}`;
+    const options = { priority: 0 };
+    return this.queue.add(() => Sagi.pushTranscript(subtitle), options)
         .then(() => {
           this.subtitleState[id] = 'successful';
           return true;
@@ -53,14 +44,12 @@ export class TranscriptQueue {
           this.subtitleState[id] = 'failed';
           return false;
         });
-    }
-    return false;
   }
 
-  async addAll(subtitles) {
-    const success = [];
-    const failure = [];
-    const addSubtitle = async (subtitle) => {
+  async addAll(subtitles: SubtitleUploadParameter[]) {
+    const success: SubtitleUploadParameter[] = [];
+    const failure: SubtitleUploadParameter[] = [];
+    const addSubtitle = async (subtitle: SubtitleUploadParameter) => {
       const result = await this.add(subtitle);
       if (result) success.push(subtitle);
       else failure.push(subtitle);
@@ -71,16 +60,16 @@ export class TranscriptQueue {
     return { success, failure };
   }
 
-  async addAllManual(subtitles) {
-    const success = [];
-    const failure = [];
-    const addSubtitle = async (subtitle) => {
-      const result = await this.add(subtitle);
+  async addAllManual(subtitles: SubtitleUploadParameter[]) {
+    const success: SubtitleUploadParameter[] = [];
+    const failure: SubtitleUploadParameter[] = [];
+    const addSubtitle = async (subtitle: SubtitleUploadParameter) => {
+      const result = await this.addManually(subtitle);
       if (result) success.push(subtitle);
       else failure.push(subtitle);
     };
 
-    await Promise.all(subtitles.map(i => addSubtitle(i, true)));
+    await Promise.all(subtitles.map(i => addSubtitle(i)));
 
     return { success, failure };
   }
