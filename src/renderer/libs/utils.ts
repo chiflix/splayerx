97e8756f35176334c1554cc95c834b0d127c7161
YuRiJin <875830716@qq.com>
2019-06-17 14:53:58 +0800

    refactor(utils): update implementation

--- src/renderer/libs/utils.ts
+++ src/renderer/libs/utils.ts
@@ -1,5 +1,5 @@
 import { createHash } from 'crypto';
-import { times } from 'lodash';
+import { times, padStart } from 'lodash';
 import { promises as fsPromises } from 'fs';
 
 /** 计算文本宽度
@@ -120,22 +120,11 @@ export async function mediaQuickHash(filePath: string) {
 
 export function timecodeFromSeconds(s: number) {
   const dt = new Date(Math.abs(s) * 1000);
-  let hours: string | number = dt.getUTCHours();
-  let minutes: string | number = dt.getUTCMinutes();
-  let seconds: string | number = dt.getUTCSeconds();
+  const hours = dt.getUTCHours();
+  const minutes = padStart(dt.getUTCMinutes().toString(), 2, '0');
+  const seconds = padStart(dt.getUTCSeconds().toString(), 2, '0');
 
-  // the above dt.get...() functions return a single digit
-  // so I prepend the zero here when needed
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
   if (hours > 0) {
-    if (hours < 10) {
-      hours = `${hours}`;
-    }
     return `${hours}:${minutes}:${seconds}`;
   }
   return `${minutes}:${seconds}`;
