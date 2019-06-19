17f6f5fef45080002638dc60b6251ba2fd0a8b0a
YuRiJin <875830716@qq.com>
2019-06-19 15:52:58 +0800

    refactor(playinfo): update parameter type

--- src/renderer/containers/VideoCanvas.vue
+++ src/renderer/containers/VideoCanvas.vue
@@ -87,7 +87,7 @@ export default {
     winAngle(val: number) {
       this.changeWindowRotate(val);
     },
-    videoId(val: string, oldVal: string) {
+    videoId(val: number, oldVal: number) {
       if (oldVal) this.saveScreenshot(oldVal);
     },
     originSrc(val: string, oldVal: string) {
@@ -265,7 +265,7 @@ export default {
       });
       windowRectService.uploadWindowBy(false, 'playing-view', this.winAngle, this.winAngleBeforeFullScreen, this.winSizeBeforeFullScreen, this.winPos);
     },
-    async saveScreenshot(videoId: string) {
+    async saveScreenshot(videoId: number) {
       const { videoElement } = this;
       const canvas = this.$refs.thumbnailCanvas;
       // todo: use metaloaded to get videoHeight and videoWidth
--- src/renderer/interfaces/IPlayInfoStorable.ts
+++ src/renderer/interfaces/IPlayInfoStorable.ts
@@ -5,31 +5,31 @@ export interface IPlayInfoStorable {
    * @description 更新video播放的信息
    * @author tanghaixiang@xindong.com
    * @date 2019-06-11
-   * @param {string} videoID
+   * @param {number} videoID
    * @param {MediaItem} data
    * @returns {Promise<boolean>} 返回是否成功更新
    * @memberof IPlayInfoStorable
    */
-  updateMediaItemBy(videoID: string, data: MediaItem): Promise<boolean>
+  updateMediaItemBy(videoID: number, data: MediaItem): Promise<boolean>
   /**
    * @description 更新最近播放列表
    * @author tanghaixiang@xindong.com
    * @date 2019-06-11
-   * @param {string} playListID
+   * @param {number} playListID
    * @param {PlaylistItem} items
    * @returns {Promise<boolean>} 返回是否成功更新
    * @memberof IPlayInfoStorable
    */
-  updateRecentPlayedBy(playListID: string, items: PlaylistItem): Promise<boolean>
+  updateRecentPlayedBy(playListID: number, items: PlaylistItem): Promise<boolean>
   /**
    * @description 删除播放列表
    * @author tanghaixiang@xindong.com
    * @date 2019-06-11
-   * @param {string} playListID
+   * @param {number} playListID
    * @returns {Promise<boolean>} 返回是否成果删除
    * @memberof IPlayInfoStorable
    */
-  deleteRecentPlayedBy(playListID: string): Promise<boolean>
+  deleteRecentPlayedBy(playListID: number): Promise<boolean>
   /**
    * @returns {Promise<PlaylistItem>}
    * 返回所有播放记录，按上次打开时间排序
