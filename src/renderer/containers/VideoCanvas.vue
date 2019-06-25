9bbe4ebc3396fd33ebac5359df6e7a0abaf2e512
YuRiJin <875830716@qq.com>
2019-06-25 16:29:17 +0800

    fix(videocanvas): reference issue

--- src/renderer/containers/VideoCanvas.vue
+++ src/renderer/containers/VideoCanvas.vue
@@ -90,6 +90,9 @@ export default {
     playListId(val: number, oldVal: number) {
       if (oldVal) this.saveScreenshot(oldVal, this.videoId);
     },
+    videoId(val: number, oldVal: number) {
+      if (!this.isFolderList) this.saveScreenshot(this.playListId, oldVal);
+    },
     originSrc(val: string, oldVal: string) {
       if (process.mas && oldVal) {
         this.$bus.$emit(`stop-accessing-${oldVal}`, oldVal);
