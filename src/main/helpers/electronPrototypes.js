17d25d49f32e4f58a81c42bd10687350718b0be2
Sin <dev@zouxin.net>
2019-06-28 18:31:38 +0800

    fix(windows): mouse drag

--- src/main/helpers/electronPrototypes.js
+++ src/main/helpers/electronPrototypes.js
@@ -1,30 +1,41 @@
-import { BrowserWindow } from 'electron';
+import { BrowserWindow, screen } from 'electron';
 
-function keepAspectRatio() {
-  clearTimeout(this._keepAspectRatioTimeout);
-  this._keepAspectRatioTimeout = setTimeout(() => {
-    if (!this._aspectRatio || this.isMaximized() || this.isFullScreen()) return;
-    const size = this.getSize();
-    const minimumSize = this.getMinimumSize();
-    const currentRatio = size[0] / size[1];
-    let [newWidth, newHeight] = size;
-    if (newWidth < minimumSize[0]) newWidth = minimumSize[0];
-    if (newHeight < minimumSize[1]) newHeight = minimumSize[1];
+function keepAspectRatio(evt, newBounds) {
+  evt.preventDefault();
+  if (!this._aspectRatio || this.isMaximized() || this.isFullScreen()) return;
+  const mousePosition = screen.getCursorScreenPoint();
+  const {
+    x, y, width, height,
+  } = this.getBounds();
+  const [oldWidth, oldHeight] = this.getSize();
+  const [minimumWidth, minimumHeight] = this.getMinimumSize();
+  let { width: newWidth, height: newHeight } = newBounds;
+  if (newWidth < minimumWidth) newWidth = minimumWidth;
+  if (newHeight < minimumHeight) newHeight = minimumHeight;
 
-    if (currentRatio > this._aspectRatio) {
-      newHeight = parseInt(newWidth / this._aspectRatio, 10);
-    } else {
-      newWidth = parseInt(newHeight * this._aspectRatio, 10);
-    }
-    this.setSize(newWidth, newHeight);
-  }, 100);
+  if (newWidth - oldWidth < newHeight - oldHeight) {
+    newHeight = parseInt(newWidth / this._aspectRatio, 10);
+  } else {
+    newWidth = parseInt(newHeight * this._aspectRatio, 10);
+  }
+  const center = { x: x + width / 2, y: y + height / 2 };
+  let anchor = { x, y }; // top left as default
+  if (mousePosition.x < center.x && mousePosition.y < center.y) {
+    anchor = { x: x + width - newWidth, y: y + height - newHeight }; // bottom right
+  } else if (mousePosition.x < center.x && mousePosition.y > center.y) {
+    anchor = { x: x + width - newWidth, y }; // top right
+  } else if (mousePosition.x > center.x && mousePosition.y < center.y) {
+    anchor = { x, y: y + height - newHeight }; // bottom left
+  }
+  const finalBounds = { width: newWidth, height: newHeight, ...anchor };
+  this.setBounds(finalBounds);
 }
 
 if (process.platform !== 'darwin') {
   BrowserWindow.prototype.setAspectRatio = function setAspectRatio(aspectRatio) {
     this._aspectRatio = aspectRatio;
     if (!this._keepAspectRatioListener) {
-      this.on('resize', keepAspectRatio);
+      this.on('will-resize', keepAspectRatio);
       this._keepAspectRatioListener = true;
     }
   };
