f10d8969d52aada8add1b87c6ac3994903eb51b0
Sin <dev@zouxin.net>
2019-07-03 14:08:03 +0800

    feat(system): log dumpfile in development env

--- src/shared/sentry.js
+++ src/shared/sentry.js
@@ -1,5 +1,6 @@
 // Be sure to call Sentry function as early as possible in the renderer process
 import Vue from 'vue';
+import { crashReporter } from 'electron';
 import * as Sentry from '@sentry/electron';
 import * as Integrations from '@sentry/integrations';
 
@@ -15,6 +16,13 @@ if (process.env.NODE_ENV !== 'development') {
         attachProps: true,
       }),
     ],
+  });
+} else {
+  crashReporter.start({
+    companyName: 'Sagittarius Tech LLC',
+    productName: 'SPlayer',
+    ignoreSystemCrashHandler: true,
+    submitURL: 'https://sentry.io/api/1449341/minidump/?sentry_key=6a94feb674b54686a6d88d7278727b7c',
   });
 }
 
