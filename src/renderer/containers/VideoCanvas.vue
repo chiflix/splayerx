aa204cde78f329b665278ecdbafd57e2785c1aa5
YuRiJin <875830716@qq.com>
2019-07-05 17:56:51 +0800

    fix(win): windows memorize function issue

--- src/renderer/containers/VideoCanvas.vue
+++ src/renderer/containers/VideoCanvas.vue
@@ -275,7 +275,7 @@ export default {
         maxVideoSize = this.winSize;
         videoSize = [this.videoHeight, this.videoWidth];
       } else {
-        maxVideoSize = this.lastWinSize;
+        maxVideoSize = this.lastWinSize[0] > 512 ? this.lastWinSize : [512, Math.round(512 / this.ratio)];
         videoSize = [this.videoWidth, this.videoHeight];
         this.videoExisted = true;
       }
