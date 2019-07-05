a6a6c44f506af274c39d426d28cc2cbf970a76ff
YuRiJin <875830716@qq.com>
2019-07-05 19:56:02 +0800

    fix(menu): volume trigger

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -28,7 +28,7 @@ import messages from '@/locales';
 import { windowRectService } from '@/services/window/WindowRectService';
 import helpers from '@/helpers';
 import { hookVue } from '@/kerning';
-import { Video as videoActions, Subtitle as subtitleActions, SubtitleManager as smActions, SubtitleManager } from '@/store/actionTypes';
+import { Video as videoActions, Subtitle as subtitleActions, SubtitleManager as smActions, SubtitleManager, Input as InputActions } from '@/store/actionTypes';
 import { log } from '@/libs/Log';
 import asyncStorage from '@/helpers/asyncStorage';
 import { videodata } from '@/store/video';
@@ -206,6 +206,7 @@ new Vue({
           click: () => {
             this.$ga.event('app', 'volume', 'keyboard');
             this.$store.dispatch(videoActions.INCREASE_VOLUME);
+            this.$store.dispatch(InputActions.KEYDOWN_UPDATE, ({ pressedKeyboardCode: 'Equal' }));
           },
         },
         {
@@ -215,6 +216,7 @@ new Vue({
           click: () => {
             this.$ga.event('app', 'volume', 'keyboard');
             this.$store.dispatch(videoActions.DECREASE_VOLUME);
+            this.$store.dispatch(InputActions.KEYDOWN_UPDATE, ({ pressedKeyboardCode: 'Minus' }));
           },
         },
       ];
