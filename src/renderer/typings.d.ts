49f20aec9da6d509839a543d4b775a0159d0f338
Yan Yifeng <yanyifeng@splayer.org>
2019-07-17 13:05:10 +0800

    chore(typings): add SPlayerAPI interface to electron

--- src/renderer/typings.d.ts
+++ src/renderer/typings.d.ts
@@ -1,4 +1,4 @@
-import Vue, { VNode } from 'vue';
+import Vue from 'vue';
 import { AxiosInstance } from 'axios';
 
 declare module '*.vue' {
@@ -35,4 +35,109 @@ declare global {
       [elem: string]: any;
     }
   }
+}
+
+declare module 'electron' {
+  enum AVSampleFormat {
+    AV_SAMPLE_FMT_NONE = -1,
+    /** unsigned 8bits */
+    AV_SAMPLE_FMT_U8,
+    /** signed 16 bits */
+    AV_SAMPLE_FMT_S16,
+    /** signed 32 bits */
+    AV_SAMPLE_FMT_S32,
+    /** float */
+    AV_SAMPLE_FMT_FLT,
+    /** double */
+    AV_SAMPLE_FMT_DBL,
+    /** unsigned 8 bits, planar */
+    AV_SAMPLE_FMT_U8P,
+    /** signed 16 bits, planar */
+    AV_SAMPLE_FMT_S16P,
+    /** signed 32 bits, planar */
+    AV_SAMPLE_FMT_S32P,
+    /** float, planar */
+    AV_SAMPLE_FMT_FLTP,
+    /** double, planar */
+    AV_SAMPLE_FMT_DBLP,
+    /** Number of sample formats. DO NOT USE if linking dynamically */
+    AV_SAMPLE_FMT_NB,
+  }
+  interface SPlayerAPI {
+    getMediaInfo(
+      /** media file path */
+      path: string,
+      callback: (
+        /** media info json string */
+        info: string,
+      ) => void,
+    ): void;
+    snapshotVideo(
+      /** source video path to snapshot from */
+      srcVideoPath: string,
+      /** image path to save */
+      imagePath: string,
+      /** time string in format of hh:mm:ss */
+      time: string,
+      /** width number in string */
+      width: string,
+      /** height number in string */
+      height: string,
+      callback: (
+        /** if not '0', an error occurred */
+        err: string,
+      ) => void,
+    ): void;
+    extractSubtitles(
+      /** source video path to extract subtitle from */
+      srcVideoPath: string,
+      /** subtitle path to save */
+      subtitlePath: string,
+      /** stream index string in the form of 0:s:0 */
+      streamIndex?: string,
+      callback: (
+        /** if not '0', an error occurred */
+        err: string,
+      ) => void,
+    ): void;
+    generateThumbnails(
+      /** source video path to extract thumbnails from */
+      srcVideoPath: string,
+      /** image path to save */
+      imagePath: string,
+      thumbnailWidth: string,
+      thumbnailHeight: string,
+      rowThumbnailCount: string,
+      columnThumbnailCount: string,
+      callback: (
+        /** if not '0', an error occurred */
+        err: string,
+      ) => void,
+    ): void;
+    grabAudioFrame(
+      /** source video path to grab audio frames from */
+      srcVideoPath: string,
+      /** only available when first sample a video */
+      seekPosition: string,
+      /** stream index string in the form of 0:s:0 */
+      streamIndex: string,
+      /** integer, usually in [1, 8], 0 for all tracks */
+      trackIndex: number,
+      /** integer, usually in [1, 8] */
+      resampleTracksCount: number,
+      /** usually in [11000, 48000] */
+      resampleFrequency: number,
+      resampleFormat: AVSampleFormat,
+      /** usually in [1, 200] */
+      frameCount: number,
+      callback: (
+        /** if not '0', an error occurred */
+        err: string,
+        frameBuffer?: Buffer,
+        frameData: string,
+      ) => void,
+    ): void;
+    stopGrabAudioFrame(): void;
+  }
+  const splayerx: SPlayerAPI;
 }
