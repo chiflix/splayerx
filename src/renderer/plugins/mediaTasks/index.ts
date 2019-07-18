fee92cf28cf34ff9ac6672e65981243d81d87c4d
Yan Yifeng <yanyifeng@splayer.org>
2019-07-18 13:50:59 +0800

    feat(mediatask): add media task exports

---
+++ src/renderer/plugins/mediaTasks/index.ts
@@ -0,0 +1,49 @@
+import { MediaInfoQueue } from './mediaInfoQueue';
+import { SnapshotSubtitleQueue } from './snapshotSubtitleQueue';
+import { ThumbnailQueue } from './thumbnailQueue';
+import { Format } from '@/interfaces/ISubtitle';
+
+const mediaInfoQueue = new MediaInfoQueue();
+const snapshotSubtitleQueue = new SnapshotSubtitleQueue();
+const thumbnailQueue = new ThumbnailQueue();
+
+export function getMediaInfo(path: string) {
+  return mediaInfoQueue.getMediaInfo(path);
+}
+export function getFormat(path: string) {
+  return mediaInfoQueue.getFormat(path);
+}
+export function getStreams(path: string) {
+  return mediaInfoQueue.getStreams(path);
+}
+export function getSubtitleStreams(path: string) {
+  return mediaInfoQueue.getSubtitleStreams(path);
+}
+export { ISubtitleStream } from './mediaInfoQueue';
+
+export function getSnapshotPath(
+  videoPath: string,
+  timeInSeconds: number,
+  width: number, height: number,
+) {
+  return snapshotSubtitleQueue.getSnapshotPath(
+    videoPath,
+    timeInSeconds,
+    width, height,
+  );
+}
+export function getSubtitlePath(videoPath: string, streamIndex: number, format: Format) {
+  return snapshotSubtitleQueue.getSubtitlePath(videoPath, streamIndex, format);
+}
+
+export function getThumbnailPath(
+  videoPath: string,
+  width: number,
+  rowCount: number, columnCount: number,
+) {
+  return thumbnailQueue.getThumbnailPath(
+    videoPath,
+    width,
+    rowCount, columnCount,
+  );
+}
