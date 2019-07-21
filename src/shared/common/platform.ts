b4f69803eeb67623c97d9e5a6aa2fed32b487fc0
YuRiJin <875830716@qq.com>
2019-07-21 16:36:13 +0800

    fix(platform): update platform esilnt

--- src/shared/common/platform.ts
+++ src/shared/common/platform.ts
@@ -1,21 +1,21 @@
 
-let _isWindows = false;
-let _isMacintosh = false;
-let _isLinux = false;
+let isWindows = false;
+let isMacintosh = false;
+let isLinux = false;
 
-const isElectronRenderer = (typeof process !== 'undefined' && typeof process.versions !== 'undefined' && typeof process.versions.electron !== 'undefined' && process.type === 'renderer');
+export const IsElectronRenderer = (typeof process !== 'undefined' && typeof process.versions !== 'undefined' && typeof process.versions.electron !== 'undefined' && process.type === 'renderer');
 
 // OS detection
 if (typeof navigator === 'object' && !isElectronRenderer) {
-	const userAgent = navigator.userAgent;
-	_isWindows = userAgent.indexOf('Windows') >= 0;
-	_isMacintosh = userAgent.indexOf('Macintosh') >= 0;
-	_isLinux = userAgent.indexOf('Linux') >= 0;
+  const userAgent = navigator.userAgent;
+  isWindows = userAgent.indexOf('Windows') >= 0;
+  isMacintosh = userAgent.indexOf('Macintosh') >= 0;
+  isLinux = userAgent.indexOf('Linux') >= 0;
 } else if (typeof process === 'object') {
-	_isWindows = (process.platform === 'win32');
-	_isMacintosh = (process.platform === 'darwin');
-	_isLinux = (process.platform === 'linux');
+  isWindows = (process.platform === 'win32');
+  isMacintosh = (process.platform === 'darwin');
+  isLinux = (process.platform === 'linux');
 }
-export const isWindows = _isWindows;
-export const isMacintosh = _isMacintosh;
-export const isLinux = _isLinux;
+export const IsWindows = isWindows;
+export const IsMacintosh = isMacintosh;
+export const IsLinux = isLinux;
