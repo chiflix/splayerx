92cbd0e16762adfc1ca9182fba1b69a28d05351a
YuRiJin <875830716@qq.com>
2019-07-21 19:19:39 +0800

    feat(locale): capsulize locale

--- src/shared/common/localize.ts
+++ src/shared/common/localize.ts
@@ -1,34 +1,50 @@
-import messages from '@/locales/index';
-import syncStorage from '@/helpers/syncStorage';
 import electron from 'electron';
 import osLocale from 'os-locale';
+import { join } from 'path';
+import { readFileSync } from 'fs';
 import { IsMacintosh } from './platform';
+import messages from '../../renderer/locales/index';
 
 const isElectronRenderer = (typeof process !== 'undefined' && typeof process.versions !== 'undefined' && typeof process.versions.electron !== 'undefined' && process.type === 'renderer');
 
-export function getSystemLocale(): string {
-  const { app } = isElectronRenderer ? electron.remote : electron;
-  let locale = IsMacintosh ? osLocale.sync() : app.getLocale();
-  locale = locale.replace('_', '-');
-  if (locale === 'zh-TW' || locale === 'zh-HK' || locale === 'zh-Hant') {
-    return 'zh-Hant';
-  }
-  if (locale.startsWith('zh')) {
-    return 'zh-Hans';
-  }
-  return 'en';
-}
+export default class Locale {
+  public displayLanguage: string;
 
-export function $t(msg: string): string {
-  const data = syncStorage.getSync('preferences');
-  const locale = data.displayLanguage ? data.displayLanguage : getSystemLocale();
-  const msgArray = msg.split('.');
-  const result = msgArray.reduce(
-    (result, prop): string | object => {
-      if (result[prop]) return result[prop];
-      return result;
-    },
-    messages[locale],
-  );
-  return typeof result === 'string' ? result : msg;
+  public constructor() {
+    this.getDisplayLanguage();
+  }
+
+  public $t(msg: string): string {
+    if (!this.displayLanguage) this.getDisplayLanguage();
+    const msgArray = msg.split('.');
+    const result = msgArray.reduce(
+      (result, prop): string | object => {
+        if (result[prop]) return result[prop];
+        return result;
+      },
+      messages[this.displayLanguage],
+    );
+    return typeof result === 'string' ? result : msg;
+  }
+
+  private getDisplayLanguage() {
+    const { app } = isElectronRenderer ? electron.remote : electron;
+    const preferencesPath = join(app.getPath('userData'), 'storage', 'preferences.json');
+    const data = JSON.parse(readFileSync(preferencesPath) as unknown as string);
+    if (data.displayLanguage) this.displayLanguage = data.displayLanguage;
+    else this.displayLanguage = this.getSystemLocale();
+  }
+
+  private getSystemLocale(): string {
+    const { app } = isElectronRenderer ? electron.remote : electron;
+    let locale = IsMacintosh ? osLocale.sync() : app.getLocale();
+    locale = locale.replace('_', '-');
+    if (locale === 'zh-TW' || locale === 'zh-HK' || locale === 'zh-Hant') {
+      return 'zh-Hant';
+    }
+    if (locale.startsWith('zh')) {
+      return 'zh-Hans';
+    }
+    return 'en';
+  }
 }
