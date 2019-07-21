8b5af5330e7990ca107524c828294375be57e45e
YuRiJin <875830716@qq.com>
2019-07-21 16:37:03 +0800

    fix(localize): fix localize eslint

--- src/shared/common/localize.ts
+++ src/shared/common/localize.ts
@@ -1,14 +1,14 @@
-import messages from '@/locales/index'
+import messages from '@/locales/index';
 import syncStorage from '@/helpers/syncStorage';
 import electron from 'electron';
 import osLocale from 'os-locale';
-import { isMacintosh } from './platform';
+import { IsMacintosh } from './platform';
 
 const isElectronRenderer = (typeof process !== 'undefined' && typeof process.versions !== 'undefined' && typeof process.versions.electron !== 'undefined' && process.type === 'renderer');
 
 export function getSystemLocale(): string {
   const { app } = isElectronRenderer ? electron.remote : electron;
-  let locale = isMacintosh ? osLocale.sync(): app.getLocale();
+  let locale = IsMacintosh ? osLocale.sync() : app.getLocale();
   locale = locale.replace('_', '-');
   if (locale === 'zh-TW' || locale === 'zh-HK' || locale === 'zh-Hant') {
     return 'zh-Hant';
@@ -19,13 +19,16 @@ export function getSystemLocale(): string {
   return 'en';
 }
 
-export function $t(msg: string): string | object {
-  console.log(syncStorage);
+export function $t(msg: string): string {
   const data = syncStorage.getSync('preferences');
   const locale = data.displayLanguage ? data.displayLanguage : getSystemLocale();
   const msgArray = msg.split('.');
-  const result = msgArray.reduce((result, prop): string | object => {
-    return result[prop] ? result[prop] : result;
-  }, messages[locale]);
+  const result = msgArray.reduce(
+    (result, prop): string | object => {
+      if (result[prop]) return result[prop];
+      return result;
+    },
+    messages[locale],
+  );
   return typeof result === 'string' ? result : msg;
 }
