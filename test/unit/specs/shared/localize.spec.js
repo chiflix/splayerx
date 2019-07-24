2c94458026e0f81a1e661cc0a04b024b1e110bfc
YuRiJin <875830716@qq.com>
2019-07-24 23:02:10 +0800

    test(localize): delete test

--- test/unit/specs/shared/localize.spec.js
+++
@@ -1,8 +0,0 @@
-import { $t } from '@/../shared/utils';
-
-describe.only('localize methods', () => {
-  it('method - $t', () => {
-    const result = $t('advance.audioDelay');
-    console.log(result);
-  });
-});
