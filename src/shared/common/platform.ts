27a5ed8a7208698aa4f7a1a2a546dea76ad3e392
YuRiJin <875830716@qq.com>
2019-07-25 13:04:01 +0800

    style(platform): update platform implement

--- src/shared/common/platform.ts
+++ src/shared/common/platform.ts
@@ -3,19 +3,11 @@ let isWindows = false;
 let isMacintosh = false;
 let isLinux = false;
 
-const isElectronRenderer = (typeof process !== 'undefined' && typeof process.versions !== 'undefined' && typeof process.versions.electron !== 'undefined' && process.type === 'renderer');
-
 // OS detection
-if (typeof navigator === 'object' && !isElectronRenderer) {
-  const userAgent = navigator.userAgent;
-  isWindows = userAgent.indexOf('Windows') >= 0;
-  isMacintosh = userAgent.indexOf('Macintosh') >= 0;
-  isLinux = userAgent.indexOf('Linux') >= 0;
-} else if (typeof process === 'object') {
-  isWindows = (process.platform === 'win32');
-  isMacintosh = (process.platform === 'darwin');
-  isLinux = (process.platform === 'linux');
-}
+isWindows = (process.platform === 'win32');
+isMacintosh = (process.platform === 'darwin');
+isLinux = (process.platform === 'linux');
+
 export const IsWindows = isWindows;
 export const IsMacintosh = isMacintosh;
 export const IsLinux = isLinux;
