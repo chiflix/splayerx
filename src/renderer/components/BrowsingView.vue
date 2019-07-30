14bea6a1548992e6aeb7fcdbc66136cfdcca1d77
Tan Yang <1029862865@qq.com>
2019-07-30 12:30:32 +0800

    style(browsingview): fix some issues about browsing menu

--- src/renderer/components/BrowsingView.vue
+++ src/renderer/components/BrowsingView.vue
@@ -106,6 +106,7 @@ export default {
       asyncTasksDone: false,
       headerToShow: true,
       menuService: null,
+      pipRestore: false,
     };
   },
   computed: {
@@ -164,6 +165,8 @@ export default {
     isPip(val: boolean) {
       this.menuService.updatePip(val);
       this.$electron.ipcRenderer.send('update-enabled', 'window.pip', true);
+      this.$electron.ipcRenderer.send('update-enabled', 'history.back', !val && this.$refs.webView.canGoBack());
+      this.$electron.ipcRenderer.send('update-enabled', 'history.forward', !val && this.$refs.webView.canGoForward());
       if (!val) {
         this.$store.dispatch('updatePipSize', this.winSize);
         this.$store.dispatch('updatePipPos', this.winPos);
@@ -189,20 +192,7 @@ export default {
         this.timer = setTimeout(() => {
           this.timeout = false;
         }, 3000);
-        const parseUrl = urlParseLax(this.currentUrl);
-        if (parseUrl.host.includes('youtube')) {
-          this.pipType = 'youtube';
-          this.youtubeAdapter();
-        } else if (parseUrl.host.includes('bilibili')) {
-          this.pipType = 'bilibili';
-          this.bilibiliAdapter();
-        } else if (parseUrl.host.includes('iqiyi')) {
-          this.pipType = 'iqiyi';
-          this.iqiyiAdapter();
-        } else {
-          this.pipType = 'others';
-          this.othersAdapter();
-        }
+        this.pipAdapter();
       }
     },
     winWidth() {
@@ -231,6 +221,10 @@ export default {
           canGoForward: this.$refs.webView.canGoForward(),
         });
       } else {
+        if (this.pipRestore) {
+          this.pipAdapter();
+          this.pipRestore = false;
+        }
         this.$refs.webView.executeJavaScript(this.calculateVideoNum, (r: number) => {
           this.hasVideo = recordIndex === 0 && !getVideoId(loadUrl).id ? false : !!r;
           this.$electron.ipcRenderer.send('update-enabled', 'window.pip', this.hasVideo);
@@ -253,18 +247,6 @@ export default {
     this.$store.dispatch('updateBrowsingPos', this.winPos);
     this.$electron.ipcRenderer.send('callMainWindowMethod', 'setAspectRatio', [0]);
     this.$electron.ipcRenderer.send('callMainWindowMethod', 'setMinimumSize', [570, 375]);
-  },
-  beforeDestroy() {
-    this.$store.dispatch(this.isPip ? 'updatePipSize' : 'updateBrowsingSize', this.winSize);
-    this.$store.dispatch(this.isPip ? 'updatePipPos' : 'updateBrowsingPos', this.winPos);
-    asyncStorage.set('browsing', {
-      pipSize: this.pipSize,
-      pipPos: this.pipPos,
-      browsingSize: this.browsingSize,
-      browsingPos: this.browsingPos,
-      barrageOpen: this.barrageOpen,
-    });
-    this.$bus.$off();
   },
   mounted() {
     this.menuService = new MenuService();
@@ -315,11 +297,22 @@ export default {
       }
     });
     this.$bus.$on('back-to-landingview', () => {
-      this.updateIsPip(false);
-      this.$router.push({
-        name: 'landing-view',
+      asyncStorage.set('browsing', {
+        pipSize: this.pipSize,
+        pipPos: this.pipPos,
+        browsingSize: this.browsingSize,
+        browsingPos: this.browsingPos,
+        barrageOpen: this.barrageOpen,
+      }).finally(() => {
+        this.$store.dispatch(this.isPip ? 'updatePipSize' : 'updateBrowsingSize', this.winSize);
+        this.$store.dispatch(this.isPip ? 'updatePipPos' : 'updateBrowsingPos', this.winPos);
+        this.updateIsPip(false);
+        this.$bus.$off();
+        this.$router.push({
+          name: 'landing-view',
+        });
+        windowRectService.uploadWindowBy(false, 'landing-view');
       });
-      windowRectService.uploadWindowBy(false, 'landing-view');
     });
     this.$refs.webView.addEventListener('load-commit', () => {
       const loadUrl = this.$refs.webView.getURL();
@@ -433,6 +426,22 @@ export default {
       updateBarrageOpen: browsingActions.UPDATE_BARRAGE_OPEN,
       updateIsPip: browsingActions.UPDATE_IS_PIP,
     }),
+    pipAdapter() {
+      const parseUrl = urlParseLax(this.currentUrl);
+      if (parseUrl.host.includes('youtube')) {
+        this.pipType = 'youtube';
+        this.youtubeAdapter();
+      } else if (parseUrl.host.includes('bilibili')) {
+        this.pipType = 'bilibili';
+        this.bilibiliAdapter();
+      } else if (parseUrl.host.includes('iqiyi')) {
+        this.pipType = 'iqiyi';
+        this.iqiyiAdapter();
+      } else {
+        this.pipType = 'others';
+        this.othersAdapter();
+      }
+    },
     handleMouseenter() {
       this.pipBtnsKeepShow = true;
       this.timeout = true;
@@ -483,6 +492,9 @@ export default {
     },
     handleUrlReload() {
       this.$refs.webView.reload();
+      if (this.isPip) {
+        this.pipRestore = true;
+      }
     },
     handleEnterPip() {
       if (this.hasVideo) {
