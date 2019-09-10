ac82b880999e55ae6a0482cedd74ed76440bb38e
Tan Yang <1029862865@qq.com>
2019-09-10 15:12:38 +0800

    style(svg): delete svg title

--- src/main/helpers/BrowserViewManager.ts
+++ src/main/helpers/BrowserViewManager.ts
@@ -223,6 +223,7 @@ export class BrowserViewManager implements IBrowserViewManager {
       }
     }
     currentView.webContents.addListener('media-started-playing', () => {
+      console.log(123);
       if (pausedChannel.includes('bilibili')) {
         let type = '';
         currentView.webContents
