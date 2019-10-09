2b0c621e862dabea01c7394029f8044855a49626
Yan Yifeng <yanyifeng@splayer.org>
2019-10-09 16:33:05 +0800

    fix(badge): fix badge text issues
    Vertical align badge's text and use padding instead of fixed width.

--- src/renderer/components/LandingView/Badge.vue
+++ src/renderer/components/LandingView/Badge.vue
@@ -6,14 +6,19 @@
 
 <style lang="scss">
 .badge.default {
-  width: 70px;
+  padding: 0 13px;
   height: 20px;
   background-color: rgba(0, 0, 0, 0.15);
   text-align: center;
+  // magic trick to center span in div (step #2)
   line-height: 20px;
+  text-align: center;
   border-radius: 4px;
 
   span {
+    // magic trick to center span in div (step #2)
+    display: inline-block;
+    vertical-align: middle;
     font-size: 11px;
     opacity: 0.27;
     color: white;
