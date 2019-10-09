6101631bac4705096f7e9f65ef91e0e076405d85
Yan Yifeng <yanyifeng@splayer.org>
2019-10-09 15:47:14 +0800

    feat(badge): add new Badge component

---
+++ src/renderer/components/LandingView/Badge.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="badge default">
+    <span><slot /></span>
+  </div>
+</template>
+
+<style lang="scss">
+.badge.default {
+  width: 70px;
+  height: 20px;
+  background-color: rgba(0, 0, 0, 0.15);
+  text-align: center;
+  line-height: 20px;
+  border-radius: 4px;
+
+  span {
+    font-size: 11px;
+    opacity: 0.27;
+    color: white;
+  }
+}
+</style>
