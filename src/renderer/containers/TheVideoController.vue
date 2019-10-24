7f96fcba7f6907275f5c1bc0936b6a6f0ba09644
YuRi Jin <875830716@qq.com>
2019-10-24 18:39:36 +0800

    fix(video): fix playbutton mouseup

--- src/renderer/containers/TheVideoController.vue
+++ src/renderer/containers/TheVideoController.vue
@@ -368,7 +368,7 @@ export default {
       this.updateMouseup({ componentName: '' });
     },
     currentMouseupWidget(newVal: string, oldVal: string) {
-      if (newVal === 'TheVideoController' && this.showSidebar) {
+      if ((newVal === 'TheVideoController' || newVal === 'PlayButton') && this.showSidebar) {
         this.updateMousedown({ componentName: '' });
         this.updateShowSidebar(false);
       }
