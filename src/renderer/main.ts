26fc294b66f795d8afbd92d485b8a59ebb9ee350
YuRi Jin <875830716@qq.com>
2019-11-04 19:02:20 +0800

    fix(shortcut): fix shortcut

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -457,6 +457,16 @@ new Vue({
       }
     });
     window.addEventListener('keydown', (e) => { // eslint-disable-line complexity
+      if (e.code === 'BracketLeft') {
+        e.preventDefault();
+        this.$store.dispatch(videoActions.DECREASE_RATE);
+      } else if (e.code === 'BracketRight') {
+        e.preventDefault();
+        this.$store.dispatch(videoActions.INCREASE_RATE);
+      } else if (e.code === 'Backslash') {
+        e.preventDefault();
+        this.$store.dispatch(videoActions.CHANGE_RATE, 1);
+      }
       switch (e.keyCode) {
         case 27:
           if (this.isFullScreen && !this.playlistDisplayState) {
@@ -469,20 +479,12 @@ new Vue({
           e.preventDefault();
           if (this.currentRouteName === 'browsing-view' && e.metaKey) {
             this.$bus.$emit('toggle-back');
-          } else {
-            this.$store.dispatch(videoActions.DECREASE_RATE);
           }
-          break;
-        case 220:
-          e.preventDefault();
-          this.$store.dispatch(videoActions.CHANGE_RATE, 1);
           break;
         case 221:
           e.preventDefault();
           if (this.currentRouteName === 'browsing-view' && e.metaKey) {
             this.$bus.$emit('toggle-forward');
-          } else {
-            this.$store.dispatch(videoActions.INCREASE_RATE);
           }
           break;
         case 187:
