bd9b5673bd95cd83e0b606fa6ed4fd67598e5800
YuRi Jin <875830716@qq.com>
2019-11-05 15:50:30 +0800

    style(preference): adjust font-size

--- src/renderer/components/Preferences/BaseCheckBox.vue
+++ src/renderer/components/Preferences/BaseCheckBox.vue
@@ -47,7 +47,7 @@ export default {
     cursor: pointer;
     padding-left: 29px;
     font-family: $font-medium;
-    font-size: 13px;
+    font-size: 14px;
     color: rgba(255,255,255,0.7);
     letter-spacing: 0.3px;
     line-height: 19px;
