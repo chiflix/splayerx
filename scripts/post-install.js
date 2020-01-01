ede7370859fb3cc99bcb642ea17c49dd6690f4e8
Sin <dev@zouxin.net>
2020-01-02 07:34:18 +0800

    feat(build): use customized electron-builder

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -13,21 +13,3 @@ exec(commands.join('&&'), (error, stdout) => {
   if (error) throw error;
   console.log(stdout);
 });
-
-// TODO: make a PR to electron-builder project
-fs.writeFileSync(
-  path.join(__dirname, '../node_modules/app-builder-lib', 'templates/appx/appxmanifest.xml'),
-  fs.readFileSync(path.join(__dirname, 'patch/appx', 'appxmanifest.xml')),
-);
-fs.writeFileSync(
-  path.join(__dirname, '../node_modules/app-builder-lib', 'out/targets/AppxTarget.js'),
-  fs.readFileSync(path.join(__dirname, 'patch/appx', 'AppxTarget.js')),
-);
-fs.writeFileSync(
-  path.join(__dirname, '../node_modules/app-builder-lib', 'out/electron/electronMac.js'),
-  fs.readFileSync(path.join(__dirname, 'patch/mac', 'electronMac.js')),
-);
-fs.writeFileSync(
-  path.join(__dirname, '../node_modules/app-builder-lib', 'out/electron/winPackager.js'),
-  fs.readFileSync(path.join(__dirname, 'patch/win', 'winPackager.js')),
-);
