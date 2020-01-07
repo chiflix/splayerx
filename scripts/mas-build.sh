562c2312611d5c5e790ef4dfe4e8527bb95f19b7
Sin <dev@zouxin.net>
2020-01-07 16:45:43 +0800

    build(mas): enable hardenedRuntime and nortarize for mas

--- scripts/mas-build.sh
+++ scripts/mas-build.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 # Reinstall the electron mas version
-ELECTRON_VERSION=`node -p -e "require('./package.json').devDependencies['@chiflix/electron']"`
-ELECTRON_VERSION=${ELECTRON_VERSION/^/''}
-force_no_cache='true' npm_config_platform=mas npm i @chiflix/electron@$ELECTRON_VERSION --save-exact
+IS_MAS_CMD="echo 'process.mas' | ./node_modules/.bin/electron -i"
+IS_MAS="$(eval "$IS_MAS_CMD")"
+if [[ $IS_MAS != *"true"* ]]; then
+    ELECTRON_VERSION=`node -p -e "require('./package.json').devDependencies['@chiflix/electron']"`
+    ELECTRON_VERSION=${ELECTRON_VERSION/^/''}
+    rm -fr ./node_modules/@chiflix/electron
+    force_no_cache='true' npm_config_platform=mas yarn add @chiflix/electron@$ELECTRON_VERSION --exact
+fi
 
 node .electron-vue/build.js
 
 rev=`git rev-list --count HEAD`
 
 electron-builder -p never -m $1 \
-    -c electron-builder-mas.json \
+    -c electron-builder.json \
     -c.mac.provisioningProfile="build/$1.provisionprofile" \
     -c.mac.bundleVersion="$rev" \
     -c.mac.category="public.app-category.entertainment"
