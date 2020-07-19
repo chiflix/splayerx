87e0ebca96e836d3aa08d7b0fcd921ede7ee05a8
Sin <dev@zouxin.net>
2020-07-20 06:42:49 +0800

    build(version): bump version to 4.9.2-beta.4

--- test/unit/specs/shared/utils.spec.js
+++ test/unit/specs/shared/utils.spec.js
@@ -9,15 +9,15 @@ describe('shared/utils', () => {
   it('should contains proper subtitle extensions', () => {
     expect(getValidSubtitleExtensions()).to.include('srt');
     expect(getValidSubtitleExtensions()).to.not.include('mp4');
-    expect(isSubtitle('filename.vtt')).toBe(true);
-    expect(isSubtitle('filename.ASS')).toBe(true);
+    expect(isSubtitle('filename.vtt')).to.be(true);
+    expect(isSubtitle('filename.ASS')).to.be(true);
   });
 
   it('should contains proper video extensions', () => {
     expect(getValidVideoExtensions()).to.include('mp4');
     expect(getValidVideoExtensions()).to.not.include('srt');
-    expect(isVideo('filename.mkv')).toBe(true);
-    expect(isVideo('filename.WMV')).toBe(true);
+    expect(isVideo('filename.mkv')).to.be(true);
+    expect(isVideo('filename.WMV')).to.be(true);
   });
 
   it('should not be able to modify', () => {
