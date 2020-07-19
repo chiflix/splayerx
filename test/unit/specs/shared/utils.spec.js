0b5fa523ded5fbd21586f169dd0391ec6b8ba68e
Sin <dev@zouxin.net>
2020-07-20 07:05:32 +0800

    feat(test): fix test

--- test/unit/specs/shared/utils.spec.js
+++ test/unit/specs/shared/utils.spec.js
@@ -9,15 +9,15 @@ describe('shared/utils', () => {
   it('should contains proper subtitle extensions', () => {
     expect(getValidSubtitleExtensions()).to.include('srt');
     expect(getValidSubtitleExtensions()).to.not.include('mp4');
-    expect(isSubtitle('filename.vtt')).to.be(true);
-    expect(isSubtitle('filename.ASS')).to.be(true);
+    expect(isSubtitle('filename.vtt')).to.be.equal(true);
+    expect(isSubtitle('filename.ASS')).to.be.equal(true);
   });
 
   it('should contains proper video extensions', () => {
     expect(getValidVideoExtensions()).to.include('mp4');
     expect(getValidVideoExtensions()).to.not.include('srt');
-    expect(isVideo('filename.mkv')).to.be(true);
-    expect(isVideo('filename.WMV')).to.be(true);
+    expect(isVideo('filename.mkv')).to.be.equal(true);
+    expect(isVideo('filename.WMV')).to.be.equal(true);
   });
 
   it('should not be able to modify', () => {
